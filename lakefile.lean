4deb08873284a627126a47c5b531af4b5039c1ca
Patrick Thomas <pthomas505@gmail.com>
2023-07-01 22:20:15 -0700

    Initial commit.

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «fOL» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «FOL» {
+  -- add any library configuration options here
+}
