29df2a1299b3856b6b0f7ca3e2e208848f5db0b0
Patrick Thomas <pthomas505@gmail.com>
2023-07-06 09:07:37 -0400

    Updates.

--- FOL/PredSub/One/Rec/PredSubOneRec.lean
+++ FOL/PredSub/One/Rec/PredSubOneRec.lean
@@ -13,46 +13,74 @@ open Formula
 /--
   The recursive simultaneous uniform substitution of a single predicate variable in a formula.
 -/
-def replacePred (P : PredName) (zs : List VarName) (H : Formula) : Formula → Formula
+def replacePred
+  (P : PredName)
+  (zs : List VarName)
+  (H : Formula) :
+  Formula → Formula
   | pred_const_ X xs => pred_const_ X xs
   | pred_var_ X xs =>
-    if X = P ∧ xs.length = zs.length
-    then fastReplaceFreeFun (Function.updateListIte id zs xs) H
-    else pred_var_ X xs
+      if X = P ∧ xs.length = zs.length
+      then fastReplaceFreeFun (Function.updateListIte id zs xs) H
+      else pred_var_ X xs
   | eq_ x y => eq_ x y
   | true_ => true_
   | false_ => false_
   | not_ phi => not_ (replacePred P zs H phi)
-  | imp_ phi psi => imp_ (replacePred P zs H phi) (replacePred P zs H psi)
-  | and_ phi psi => and_ (replacePred P zs H phi) (replacePred P zs H psi)
-  | or_ phi psi => or_ (replacePred P zs H phi) (replacePred P zs H psi)
-  | iff_ phi psi => iff_ (replacePred P zs H phi) (replacePred P zs H psi)
+  | imp_ phi psi =>
+      imp_
+      (replacePred P zs H phi)
+      (replacePred P zs H psi)
+  | and_ phi psi =>
+      and_
+      (replacePred P zs H phi)
+      (replacePred P zs H psi)
+  | or_ phi psi =>
+      or_
+      (replacePred P zs H phi)
+      (replacePred P zs H psi)
+  | iff_ phi psi =>
+      iff_
+      (replacePred P zs H phi)
+      (replacePred P zs H psi)
   | forall_ x phi => forall_ x (replacePred P zs H phi)
   | exists_ x phi => exists_ x (replacePred P zs H phi)
 
 
-def admitsPredAux (P : PredName) (zs : List VarName) (H : Formula) : Finset VarName → Formula → Prop
+def admitsPredAux
+  (P : PredName)
+  (zs : List VarName)
+  (H : Formula) :
+  Finset VarName → Formula → Prop
   | _, pred_const_ _ _ => True
   | binders, pred_var_ X ts =>
-    if X = P ∧ ts.length = zs.length
-    then
-      admitsFun (Function.updateListIte id zs ts) H ∧
-          /-
-            Suppose F is the formula that the predicate X ts occurs in.
-            Ensures that the free variables in H that are not being replaced by a variable in ts do not become bound variables in F. The bound variables in F are in the 'binders' set.
-            The zs are the free variables in H that are being replaced by the variables in ts.
-           (is_free_in x H ∧ x ∉ zs) := x is a free variable in H that is not being replaced by a variable in ts.
-          -/
-        ∀ x : VarName, x ∈ binders → ¬(isFreeIn x H ∧ x ∉ zs)
-    else True
+      if X = P ∧ ts.length = zs.length
+      then
+        admitsFun (Function.updateListIte id zs ts) H ∧
+            /-
+              Suppose F is the formula that the predicate X ts occurs in.
+              Ensures that the free variables in H that are not being replaced by a variable in ts do not become bound variables in F. The bound variables in F are in the 'binders' set.
+              The zs are the free variables in H that are being replaced by the variables in ts.
+            (is_free_in x H ∧ x ∉ zs) := x is a free variable in H that is not being replaced by a variable in ts.
+            -/
+          ∀ x : VarName, x ∈ binders → ¬(isFreeIn x H ∧ x ∉ zs)
+      else True
   | _, eq_ _ _ => True
   | _, true_ => True
   | _, false_ => True
   | binders, not_ phi => admitsPredAux P zs H binders phi
-  | binders, imp_ phi psi => admitsPredAux P zs H binders phi ∧ admitsPredAux P zs H binders psi
-  | binders, and_ phi psi => admitsPredAux P zs H binders phi ∧ admitsPredAux P zs H binders psi
-  | binders, or_ phi psi => admitsPredAux P zs H binders phi ∧ admitsPredAux P zs H binders psi
-  | binders, iff_ phi psi => admitsPredAux P zs H binders phi ∧ admitsPredAux P zs H binders psi
+  | binders, imp_ phi psi =>
+      admitsPredAux P zs H binders phi ∧
+      admitsPredAux P zs H binders psi
+  | binders, and_ phi psi =>
+      admitsPredAux P zs H binders phi ∧
+      admitsPredAux P zs H binders psi
+  | binders, or_ phi psi =>
+      admitsPredAux P zs H binders phi ∧
+      admitsPredAux P zs H binders psi
+  | binders, iff_ phi psi =>
+      admitsPredAux P zs H binders phi ∧
+      admitsPredAux P zs H binders psi
   | binders, forall_ x phi => admitsPredAux P zs H (binders ∪ {x}) phi
   | binders, exists_ x phi => admitsPredAux P zs H (binders ∪ {x}) phi
 
@@ -82,20 +110,21 @@ theorem pred_sub_single_aux
   induction F generalizing binders V
   case pred_const_ X xs =>
     unfold replacePred
-    simp only [Holds]
+    unfold Holds
+    simp
   case pred_var_ X xs =>
       unfold admitsPredAux at h1
+
       unfold replacePred
       simp only [Holds]
-      --simp only [Interpretation.pred_var_]
       simp
       split_ifs at h1
       case inl c1 =>
+        unfold admitsFun at h1
         simp at h1
+
         cases h1
         case intro h1_left h1_right =>
-          unfold admitsFun at h1_left
-
           obtain s1 := substitution_fun_theorem D I V (Function.updateListIte id zs xs) H h1_left
           simp only [Function.updateListIte_comp] at s1
           simp only [Function.comp.right_id] at s1
