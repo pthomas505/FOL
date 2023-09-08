7aabf0bd11f2b7a6d7bd314b0e0a98a2fe723f56
Patrick Thomas <pthomas505@gmail.com>
2023-09-08 09:56:05 -0700

    Updates.

--- FOL/Sub/All/Rec/SubAllRecChangeReplaceFree.lean
+++ FOL/Sub/All/Rec/SubAllRecChangeReplaceFree.lean
@@ -17,15 +17,15 @@ lemma list_str_max_len_mem
     simp at h1
   case cons hd tl ih =>
     simp at h1
+
     unfold List.str_max_len
     cases h1
     case inl c1 =>
       simp only [c1]
       simp
     case inr c1 =>
-      specialize ih c1
       trans List.str_max_len tl
-      · exact ih
+      · exact ih c1
       · simp
 
 def variant
@@ -37,17 +37,42 @@ def variant
   have : List.str_max_len l + 1 - (String.length s + String.length (Char.toString c)) < List.str_max_len l + 1 - String.length s :=
     by
     have s1 : (Char.toString c).length = 1
-    simp only [String.length]
-    unfold Char.toString
-    simp
+    rfl
+
     simp only [s1]
     simp
     obtain s2 := list_str_max_len_mem s l h
-    refine Iff.mpr (tsub_lt_tsub_iff_right s2) ?_
+    simp only [tsub_lt_tsub_iff_right s2]
     simp
   variant (s ++ c.toString) c l
   else s
   termination_by variant s _ l => List.str_max_len l + 1 - s.length
 
+lemma variant_spec
+  (s : String)
+  (c : Char)
+  (l : List String) :
+  ¬ variant s c l ∈ l :=
+  if h : s ∈ l
+  then
+  have : List.str_max_len l + 1 - (String.length s + String.length (Char.toString c)) < List.str_max_len l + 1 - String.length s :=
+    by
+    have s1 : (Char.toString c).length = 1
+    rfl
 
-#eval variant "a" '+' ["a+", "a"]
+    simp only [s1]
+    simp
+    obtain s2 := list_str_max_len_mem s l h
+    simp only [tsub_lt_tsub_iff_right s2]
+    simp
+  by
+    unfold variant
+    simp
+    simp only [if_pos h]
+    apply variant_spec
+  else by
+    unfold variant
+    simp
+    simp [if_neg h]
+    exact h
+  termination_by variant_spec s _ l => List.str_max_len l + 1 - s.length
