9951e37262d8a7a9eb6498f5120fef2d6773cf25
Patrick Thomas <pthomas505@gmail.com>
2023-09-20 23:07:23 -0700

    Updates.

--- FOL/Sub/All/Rec/SubAllRecChangeReplaceFree.lean
+++ FOL/Sub/All/Rec/SubAllRecChangeReplaceFree.lean
@@ -415,3 +415,351 @@ theorem sub_valid
   intros D I V E
   simp only [substitution_fun_theorem]
   apply h1
+
+
+def replacePredFun
+  (c : Char)
+  (τ : PredName → ℕ → Option (List VarName × Formula)) :
+  Formula → Formula
+  | pred_const_ X xs => pred_const_ X xs
+  | pred_var_ X xs =>
+      let opt := τ X xs.length
+      if h : Option.isSome opt
+      then
+        let val := Option.get opt h
+        let zs := val.fst
+        let H := val.snd
+        if xs.length = zs.length
+        then sub (Function.updateListIte id zs xs) c H
+        else pred_var_ X xs
+      else pred_var_ X xs
+  | eq_ x y => eq_ x y
+  | true_ => true_
+  | false_ => false_
+  | not_ phi => not_ (replacePredFun c τ phi)
+  | imp_ phi psi =>
+      imp_
+      (replacePredFun c τ phi)
+      (replacePredFun c τ psi)
+  | and_ phi psi =>
+      and_
+      (replacePredFun c τ phi)
+      (replacePredFun c τ psi)
+  | or_ phi psi =>
+      or_
+      (replacePredFun c τ phi)
+      (replacePredFun c τ psi)
+  | iff_ phi psi =>
+      iff_
+      (replacePredFun c τ phi)
+      (replacePredFun c τ psi)
+  | forall_ x phi => forall_ x (replacePredFun c τ phi)
+  | exists_ x phi => exists_ x (replacePredFun c τ phi)
+  | def_ X xs => def_ X xs
+
+
+def admitsPredFunAux
+  (τ : PredName → ℕ → Option (List VarName × Formula)) :
+  Finset VarName → Formula → Prop
+  | _, pred_const_ _ _ => True
+  | binders, pred_var_ X xs =>
+      let opt := τ X xs.length
+      if h : Option.isSome opt
+      then
+        let val := Option.get opt h
+        let zs := val.fst
+        let H := val.snd
+        if xs.length = zs.length
+        then ∀ x : VarName, x ∈ binders → ¬ (isFreeIn x H ∧ x ∉ zs)
+        else True
+      else True
+  | _, true_ => True
+  | _, false_ => True
+  | _, eq_ _ _ => True
+  | binders, not_ phi => admitsPredFunAux τ binders phi
+  | binders, imp_ phi psi =>
+      admitsPredFunAux τ binders phi ∧
+      admitsPredFunAux τ binders psi
+  | binders, and_ phi psi =>
+      admitsPredFunAux τ binders phi ∧
+      admitsPredFunAux τ binders psi
+  | binders, or_ phi psi =>
+      admitsPredFunAux τ binders phi ∧
+      admitsPredFunAux τ binders psi
+  | binders, iff_ phi psi =>
+      admitsPredFunAux τ binders phi ∧
+      admitsPredFunAux τ binders psi
+  | binders, forall_ x phi => admitsPredFunAux τ (binders ∪ {x}) phi
+  | binders, exists_ x phi => admitsPredFunAux τ (binders ∪ {x}) phi
+  | _, def_ _ _ => True
+
+
+theorem predSub_aux
+  (D : Type)
+  (I : Interpretation D)
+  (V V' : VarAssignment D)
+  (E : Env)
+  (c : Char)
+  (τ : PredName → ℕ → Option (List VarName × Formula))
+  (binders : Finset VarName)
+  (F : Formula)
+  (h1 : admitsPredFunAux τ binders F)
+  (h2 : ∀ x : VarName, x ∉ binders → V x = V' x) :
+  Holds D
+    ⟨
+      I.nonempty,
+      I.pred_const_,
+      fun (X : PredName) (ds : List D) =>
+      let opt := τ X ds.length
+      if h : Option.isSome opt
+      then
+        let val := Option.get opt h
+        let zs := val.fst
+        let H := val.snd
+        if ds.length = zs.length
+        then Holds D I (Function.updateListIte V' zs ds) E H
+        else I.pred_var_ X ds
+      else I.pred_var_ X ds
+    ⟩ 
+    V E F ↔ Holds D I V E (replacePredFun c τ F) :=
+  by
+  induction F generalizing binders V
+  case pred_const_ X xs =>
+    unfold replacePredFun
+    simp only [Holds]
+  case pred_var_ X xs =>
+    unfold admitsPredFunAux at h1
+    simp at h1
+
+    unfold replacePredFun
+    simp only [Holds]
+    simp
+    split_ifs at h1
+    case inl c1 =>
+      cases h1
+      case inl c2 =>
+        split_ifs
+        simp only [Holds]
+      case inr c2 =>
+        split_ifs
+        case inl c3 =>
+          let opt := τ X xs.length
+          let val := Option.get opt c1
+          let zs := val.fst
+          let H := val.snd
+          obtain s1 := substitution_fun_theorem D I V E (Function.updateListIte id zs xs) c H
+          simp only [Function.updateListIte_comp] at s1
+          simp at s1
+
+          have s2 : Holds D I (Function.updateListIte V zs (List.map V xs)) E H ↔ Holds D I (Function.updateListIte V' zs (List.map V xs)) E H
+          {
+            apply Holds_coincide_Var
+            intro v a1
+            by_cases s2_c1 : v ∈ zs
+            · apply Function.updateListIte_mem_eq_len V V' v zs (List.map V xs) s2_c1
+              simp
+              simp only [← c3]
+            · by_cases s2_c2 : v ∈ binders
+              · specialize c2 v s2_c2 a1
+                contradiction
+              · specialize h2 v s2_c2
+                apply Function.updateListIte_mem'
+                exact h2
+          }
+          simp only [s2] at s1
+          simp only [s1]
+        case inr c3 =>
+          simp only [Holds]
+    case inr c1 =>
+      split_ifs
+      simp only [Holds]
+  case eq_ x y =>
+    unfold replacePredFun
+    simp only [Holds]
+  case true_ | false_ =>
+    unfold replacePredFun
+    simp only [Holds]
+  case not_ phi phi_ih =>
+    unfold admitsPredFunAux at h1
+
+    unfold replacePredFun
+    simp only [Holds]
+    congr! 1
+    exact phi_ih V binders h1 h2
+  case
+      imp_ phi psi phi_ih psi_ih
+    | and_ phi psi phi_ih psi_ih
+    | or_ phi psi phi_ih psi_ih
+    | iff_ phi psi phi_ih psi_ih =>
+    unfold admitsPredFunAux at h1
+
+    unfold replacePredFun
+    simp only [Holds]
+
+    cases h1
+    case intro h1_left h1_right =>
+      congr! 1
+      · exact phi_ih V binders h1_left h2
+      · exact psi_ih V binders h1_right h2
+  case forall_ x phi phi_ih | exists_ x phi phi_ih =>
+    unfold admitsPredFunAux at h1
+
+    unfold replacePredFun
+    simp only [Holds]
+    first | apply forall_congr' | apply exists_congr
+    intro d
+    apply phi_ih (Function.updateIte V x d) (binders ∪ {x}) h1
+    intro v a1
+    unfold Function.updateIte
+    simp at a1
+    push_neg at a1
+    cases a1
+    case h.intro a1_left a1_right =>
+      simp only [if_neg a1_right]
+      exact h2 v a1_left
+  case def_ X xs =>
+    cases E
+    case nil =>
+      unfold replacePredFun
+      simp only [Holds]
+    case cons hd tl =>
+      unfold replacePredFun
+      simp only [Holds]
+      split_ifs
+      case _ c1 =>
+        apply Holds_coincide_PredVar
+        · simp
+        · simp only [predVarOccursIn_iff_mem_predVarSet]
+          simp only [hd.h2]
+          simp
+      case _ c1 =>
+        apply Holds_coincide_PredVar
+        · simp
+        · unfold predVarOccursIn
+          simp
+
+
+def Formula.predVarSet' : Formula → Finset (PredName × List VarName)
+  | pred_const_ _ _ => ∅
+  | pred_var_ X xs => {(X, xs)}
+  | eq_ _ _ => ∅
+  | true_ => ∅
+  | false_ => ∅
+  | not_ phi => phi.predVarSet'
+  | imp_ phi psi => phi.predVarSet' ∪ psi.predVarSet'
+  | and_ phi psi => phi.predVarSet' ∪ psi.predVarSet'
+  | or_ phi psi => phi.predVarSet' ∪ psi.predVarSet'
+  | iff_ phi psi => phi.predVarSet' ∪ psi.predVarSet'
+  | forall_ _ phi => phi.predVarSet'
+  | exists_ _ phi => phi.predVarSet'
+  | def_ _ _ => ∅
+
+
+instance {α} (S : Finset α) : Decidable (∃ a, a ∈ S) := inferInstanceAs (Decidable S.Nonempty)
+
+
+def Formula.length : Formula → ℕ
+  | pred_const_ _ _ => 0
+  | pred_var_ _ _ => 0
+  | eq_ _ _ => 0
+  | true_ => 0
+  | false_ => 0
+  | not_ phi => 1 + phi.length
+  | imp_ phi psi => 1 + phi.length + psi.length
+  | and_ phi psi => 1 + phi.length + psi.length
+  | or_ phi psi => 1 + phi.length + psi.length
+  | iff_ phi psi => 1 + phi.length + psi.length
+  | forall_ _ phi => 1 + phi.length
+  | exists_ _ phi => 1 + phi.length
+  | def_ _ _ => 0
+
+
+lemma sub_formula_length_same
+  (σ : VarName → VarName)
+  (c : Char)
+  (F : Formula) :
+  (sub σ c F).length = F.length :=
+  by
+  induction F generalizing σ
+  case not_ phi phi_ih =>
+    unfold sub
+    unfold Formula.length
+    simp only [phi_ih]
+  case
+      imp_ phi psi phi_ih psi_ih
+    | and_ phi psi phi_ih psi_ih
+    | or_ phi psi phi_ih psi_ih
+    | iff_ phi psi phi_ih psi_ih =>
+    unfold sub
+    unfold Formula.length
+    simp only [phi_ih σ]
+    simp only [psi_ih σ]
+  case forall_ x phi phi_ih | exists_ x phi_ih =>
+    unfold sub
+    unfold Formula.length
+    simp
+    apply phi_ih
+  all_goals
+    unfold sub
+    unfold Formula.length
+    simp
+
+/-
+def subPred
+  (c : Char)
+  (τ : PredName → ℕ → Option (List VarName × Formula)) :
+  Formula → Formula
+  | pred_const_ X xs => pred_const_ X xs
+  | pred_var_ X xs =>
+      let opt := τ X xs.length
+      if h : Option.isSome opt
+      then
+        let val := Option.get opt h
+        let zs := val.fst
+        let H := val.snd
+        if xs.length = zs.length
+        then sub (Function.updateListIte id zs xs) c H
+        else pred_var_ X xs
+      else pred_var_ X xs
+  | eq_ x y => eq_ x y
+  | true_ => true_
+  | false_ => false_
+  | not_ phi => not_ (subPred c τ phi)
+  | imp_ phi psi =>
+      imp_
+      (subPred c τ phi)
+      (subPred c τ psi)
+  | and_ phi psi =>
+      and_
+      (subPred c τ phi)
+      (subPred c τ psi)
+  | or_ phi psi =>
+      or_
+      (subPred c τ phi)
+      (subPred c τ psi)
+  | iff_ phi psi =>
+      iff_
+      (subPred c τ phi)
+      (subPred c τ psi)
+  | forall_ x phi =>
+      let x' : VarName :=
+        if h1 : ∃ y : (PredName × ℕ), y ∈ phi.predVarSet
+        then
+          let opt := (τ y.fst y.snd)
+          if h2 : Option.isSome opt
+          then
+            let val := Option.get opt h2
+            let zs := val.fst
+            let H := val.snd
+            if y.snd = zs.length
+            then
+              if ¬ (isFreeIn x H ∧ x ∉ zs)
+              then x
+              else variant x c (subPred c τ phi).freeVarSet
+            else x
+          else x
+        else x
+      forall_ x' (sub (Function.updateIte id x x') c (subPred c τ phi))
+  | exists_ x phi => exists_ x (subPred c τ phi)
+  | def_ X xs => def_ X xs
+-/
