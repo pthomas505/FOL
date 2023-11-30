75f7cc1c91191f30177b6cdbf8beee14a7c83429
Patrick Thomas <pthomas505@gmail.com>
2023-11-29 20:26:40 -0800

    Updates.

---
+++ FOL/All.lean
@@ -0,0 +1,32 @@
+import FOL.Tactics
+import FOL.FunctionUpdateITE
+import FOL.List
+import FOL.Finset
+import FOL.Fresh
+
+import FOL.NV.Fresh
+import FOL.NV.Formula
+import FOL.NV.Binders
+import FOL.NV.Definition
+import FOL.NV.Semantics
+
+import FOL.NV.Sub.One.Ind.SubOneInd
+import FOL.NV.Sub.One.Rec.SubOneRecReplaceFree
+import FOL.NV.Sub.One.Rec.SubOneRecAdmits
+
+import FOL.NV.Sub.All.Ind.SubAllInd
+import FOL.NV.Sub.All.Rec.SubAllRecReplaceAll
+import FOL.NV.Sub.All.Rec.SubAllRecReplaceFree
+import FOL.NV.Sub.All.Rec.SubAllRecAdmits
+import FOL.NV.Sub.All.Rec.SubAllRecFresh
+
+import FOL.NV.PropSub.All.Rec.PropSubAllRec
+
+import FOL.NV.PredSub.One.Ind.PredSubOneInd
+import FOL.NV.PredSub.One.Rec.PredSubOneRec
+
+/-
+import FOL.NV.PredSub.All.Rec.PredSubAllRec
+import FOL.NV.PredSub.All.Rec.PredSubAllRecFresh
+import FOL.NV.PredSub.All.Rec.PredSubAllRecOption
+-/
