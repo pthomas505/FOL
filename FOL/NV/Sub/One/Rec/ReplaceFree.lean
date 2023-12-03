3449bc17005f71ef3aa5acd813a59ed49e59b537
Patrick Thomas <pthomas505@gmail.com>
2023-12-02 20:44:01 -0800

    Updates.

--- FOL/NV/Sub/One/Rec/ReplaceFree.lean
+++ FOL/NV/Sub/One/Rec/ReplaceFree.lean
@@ -127,40 +127,33 @@ theorem replaceFreeAux_mem_binders
   replaceFreeAux v t binders F = F :=
   by
   induction F generalizing binders
-  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+  any_goals
     simp only [replaceFreeAux]
-    congr!
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+    simp
     simp only [List.map_eq_self_iff]
     simp
     intro x _ a2 a3
     subst a2
     contradiction
   case eq_ x y =>
-    simp only [replaceFreeAux]
     simp
     constructor
     case left | right =>
       intro a1 a2
       subst a1
       contradiction
-  case true_ | false_ =>
-    rfl
   case not_ phi phi_ih =>
-    simp only [replaceFreeAux]
-    congr!
-    exact phi_ih binders h1
+    tauto
   case
       imp_ phi psi phi_ih psi_ih
     | and_ phi psi phi_ih psi_ih
     | or_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
-    simp only [replaceFreeAux]
-    congr!
-    · exact phi_ih binders h1
-    · exact psi_ih binders h1
+    simp
+    tauto
   case forall_ x phi phi_ih | exists_ x phi phi_ih =>
-    simp only [replaceFreeAux]
-    congr!
+    simp
     apply phi_ih
     simp
     left
@@ -176,9 +169,10 @@ theorem replaceFreeAux_eq_fastReplaceFree
     fastReplaceFree v t F :=
   by
   induction F generalizing binders
-  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+  any_goals
     simp only [replaceFreeAux]
     simp only [fastReplaceFree]
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
     congr!
     case _ x =>
       constructor
@@ -189,8 +183,6 @@ theorem replaceFreeAux_eq_fastReplaceFree
         subst a1
         tauto
   case eq_ x y =>
-    simp only [replaceFreeAux]
-    simp only [fastReplaceFree]
     congr!
     case _ | _ =>
       constructor
@@ -200,29 +192,19 @@ theorem replaceFreeAux_eq_fastReplaceFree
         intro a1
         subst a1
         tauto
-  case true_ | false_ =>
-    rfl
   case not_ phi phi_ih =>
-    simp only [replaceFreeAux]
-    simp only [fastReplaceFree]
-    congr! 1
-    exact phi_ih binders h1
+    tauto
   case
       imp_ phi psi phi_ih psi_ih
     | and_ phi psi phi_ih psi_ih
     | or_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
-    simp only [replaceFreeAux]
-    simp only [fastReplaceFree]
-    congr! 1
-    · exact phi_ih binders h1
-    · exact psi_ih binders h1
+    simp
+    tauto
   case forall_ x phi phi_ih | exists_ x phi phi_ih =>
-    simp only [replaceFreeAux]
-    simp only [fastReplaceFree]
     split_ifs
     case pos c1 =>
-      congr!
+      congr! 1
       apply replaceFreeAux_mem_binders
       simp
       right
@@ -251,28 +233,23 @@ theorem fastReplaceFree_self
   fastReplaceFree v v F = F :=
   by
   induction F
-  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+  any_goals
     simp only [fastReplaceFree]
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
     simp
     simp only [List.map_eq_self_iff]
     simp
   case eq_ x y =>
-    simp only [fastReplaceFree]
     simp
-  case true_ | false_ =>
-    rfl
   case not_ phi phi_ih =>
-    simp only [fastReplaceFree]
     congr!
   case
       imp_ phi psi phi_ih psi_ih
     | and_ phi psi phi_ih psi_ih
     | or_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
-    simp only [fastReplaceFree]
     congr!
   case forall_ x phi phi_ih | exists_ x phi phi_ih =>
-    simp only [fastReplaceFree]
     simp
     intro _
     exact phi_ih
@@ -285,10 +262,11 @@ theorem not_free_in_fastReplaceFree_self
   fastReplaceFree v t F = F :=
   by
   induction F
-  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+  any_goals
     simp only [isFreeIn] at h1
 
     simp only [fastReplaceFree]
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
     simp
     simp only [List.map_eq_self_iff]
     simp
@@ -296,47 +274,20 @@ theorem not_free_in_fastReplaceFree_self
     subst a2
     contradiction
   case eq_ x y =>
-    simp only [isFreeIn] at h1
-    push_neg at h1
-
-    cases h1
-    case intro h1_left h1_right =>
-      simp only [fastReplaceFree]
-      congr!
-      · simp
-        tauto
-      · simp
-        tauto
-  case true_ | false_ =>
-    rfl
+    simp
+    tauto
   case not_ phi phi_ih =>
-    simp only [isFreeIn] at h1
-
-    simp only [fastReplaceFree]
     tauto
   case
       imp_ phi psi phi_ih psi_ih
     | and_ phi psi phi_ih psi_ih
     | or_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
-    simp only [isFreeIn] at h1
-    push_neg at h1
-
-    cases h1
-    case intro h1_left h1_right =>
-      simp only [fastReplaceFree]
-      congr!
-      · exact phi_ih h1_left
-      · exact psi_ih h1_right
-  case forall_ x phi phi_ih | exists_ x phi phi_ih =>
-    simp only [isFreeIn] at h1
-    push_neg at h1
-
-    simp only [fastReplaceFree]
     simp
-    intro a1
-    apply phi_ih
-    exact h1 a1
+    tauto
+  case forall_ x phi phi_ih | exists_ x phi phi_ih =>
+    simp
+    tauto
 
 
 theorem fastReplaceFree_inverse
@@ -346,10 +297,11 @@ theorem fastReplaceFree_inverse
   fastReplaceFree t v (fastReplaceFree v t F) = F :=
   by
   induction F
-  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+  any_goals
     simp only [occursIn] at h1
 
     simp only [fastReplaceFree]
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
     congr!
     simp
     simp only [List.map_eq_self_iff]
@@ -365,21 +317,14 @@ theorem fastReplaceFree_inverse
       subst a2
       contradiction
   case eq_ x y =>
-    simp only [occursIn] at h1
     push_neg at h1
 
     cases h1
     case intro h1_left h1_right =>
-      simp only [fastReplaceFree]
       congr!
       · split_ifs <;> tauto
       · split_ifs <;> tauto
-  case true_ | false_ =>
-    rfl
   case not_ phi phi_ih =>
-    simp only [occursIn] at h1
-
-    simp only [fastReplaceFree]
     congr!
     exact phi_ih h1
   case
@@ -387,22 +332,18 @@ theorem fastReplaceFree_inverse
     | and_ phi psi phi_ih psi_ih
     | or_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
-    simp only [occursIn] at h1
     push_neg at h1
 
     cases h1
     case intro h1_left h1_right =>
-      simp only [fastReplaceFree]
       congr!
       · exact phi_ih h1_left
       · exact psi_ih h1_right
   case forall_ x phi phi_ih | exists_ x phi phi_ih =>
-    simp only [occursIn] at h1
     push_neg at h1
 
     cases h1
     case intro h1_left h1_right =>
-      simp only [fastReplaceFree]
       split_ifs
       case pos c1 =>
         simp only [fastReplaceFree]
@@ -425,47 +366,25 @@ theorem not_isFreeIn_fastReplaceFree
   ¬ isFreeIn v (fastReplaceFree v t F) :=
   by
   induction F
+  any_goals
+    simp only [fastReplaceFree]
+    simp only [isFreeIn]
   case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
-    simp only [fastReplaceFree]
-    simp only [isFreeIn]
-    push_neg
     simp
-    intro x _
-    split_ifs
-    case pos c1 =>
-      tauto
-    case neg c1 =>
-      tauto
+    intro x
+    split_ifs <;> tauto
   case eq_ x y =>
-    simp only [fastReplaceFree]
-    simp only [isFreeIn]
-    push_neg
-    constructor
-    case left | right =>
-      split_ifs
-      case pos c1 =>
-        exact h1
-      case neg c1 =>
-        exact c1
+    split_ifs <;> tauto
   case true_ | false_ =>
-    simp only [fastReplaceFree]
-    simp only [isFreeIn]
     simp
   case not_ phi phi_ih =>
-    simp only [fastReplaceFree]
-    simp only [isFreeIn]
     exact phi_ih
   case
       imp_ phi psi phi_ih psi_ih
     | and_ phi psi phi_ih psi_ih
     | or_ phi psi phi_ih psi_ih
     | iff_ phi psi phi_ih psi_ih =>
-    simp only [fastReplaceFree]
-    simp only [isFreeIn]
-    push_neg
-    constructor
-    · exact phi_ih
-    · exact psi_ih
+    tauto
   case forall_ x phi phi_ih | exists_ x phi phi_ih =>
     simp only [fastReplaceFree]
     split_ifs
