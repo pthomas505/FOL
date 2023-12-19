41964b27af0bb7d2cfc5adc704efa249f0ec4b10
Patrick Thomas <pthomas505@gmail.com>
2023-12-18 23:40:26 -0500

    Updates.

--- FOL/NV/Sub/Pred/All/Rec/SubOptionFresh.lean
+++ FOL/NV/Sub/Pred/All/Rec/SubOptionFresh.lean
@@ -27,7 +27,8 @@ def predVarFreeVarSet
 def subPredAlphaAux
   (c : Char)
   (τ : PredName → ℕ → Option (List VarName × Formula))
-  (σ : VarName → VarName) :
+  (σ : VarName → VarName)
+  (S T : Finset VarName) :
   Formula → Formula
   | pred_const_ X xs => pred_const_ X (xs.map σ)
   | pred_var_ X xs =>
@@ -45,36 +46,36 @@ def subPredAlphaAux
   | true_ => true_
   | false_ => false_
   | not_ phi =>
-      not_ (subPredAlphaAux c τ σ phi)
+      not_ (subPredAlphaAux c τ σ S T phi)
   | imp_ phi psi =>
       imp_
-      (subPredAlphaAux c τ σ phi)
-      (subPredAlphaAux c τ σ psi)
+      (subPredAlphaAux c τ σ S T phi)
+      (subPredAlphaAux c τ σ S T psi)
   | and_ phi psi =>
       and_
-      (subPredAlphaAux c τ σ phi)
-      (subPredAlphaAux c τ σ psi)
+      (subPredAlphaAux c τ σ S T phi)
+      (subPredAlphaAux c τ σ S T psi)
   | or_ phi psi =>
       or_
-      (subPredAlphaAux c τ σ phi)
-      (subPredAlphaAux c τ σ psi)
+      (subPredAlphaAux c τ σ S T phi)
+      (subPredAlphaAux c τ σ S T psi)
   | iff_ phi psi =>
       iff_
-      (subPredAlphaAux c τ σ phi)
-      (subPredAlphaAux c τ σ psi)
+      (subPredAlphaAux c τ σ S T phi)
+      (subPredAlphaAux c τ σ S T psi)
   | forall_ x phi =>
       let x' : VarName :=
-        if x ∈ phi.predVarSet.biUnion (predVarFreeVarSet τ)
-        then fresh x c ((subPredAlphaAux c τ (Function.updateITE σ x x) phi).predVarSet.biUnion (predVarFreeVarSet τ))
+        if x ∈ (((FOL.NV.Sub.Var.All.Rec.subFresh (Function.updateITE σ x x) c phi).freeVarSet) ∪ Finset.biUnion (predVarSet phi) (predVarFreeVarSet τ))
+        then fresh x c (Finset.image (Function.updateITE σ x x) (freeVarSet phi))
         else x
-      forall_ x' (subPredAlphaAux c τ (Function.updateITE σ x x') phi)
+      forall_ x' (subPredAlphaAux c τ (Function.updateITE σ x x') S T phi)
   | exists_ x phi =>
       let vs : Finset VarName := sorry
       let x' : VarName :=
         if x ∈ vs
         then fresh x c vs
         else x
-      exists_ x' (subPredAlphaAux c τ (Function.updateITE σ x x') phi)
+      exists_ x' (subPredAlphaAux c τ (Function.updateITE σ x x') S T phi)
   | def_ X xs => def_ X (xs.map σ)
 
 
@@ -96,6 +97,7 @@ example
   (c : Char)
   (τ : PredName → ℕ → Option (List VarName × Formula))
   (σ : VarName → VarName)
+  (S T : Finset VarName)
   (F : Formula)
   (h1 : ∀ (x : VarName), isFreeIn x F → V' x = V (σ x))
   (h2 : ∀ (x : VarName), x ∈ F.predVarSet.biUnion (predVarFreeVarSet τ) → V'' x = V x)
@@ -112,12 +114,13 @@ example
         then Holds D I (Function.updateListITE V'' zs ds) E H
         else I.pred_var_ X ds
       else I.pred_var_ X ds) )
-  Holds D I'
-    V' E F ↔ Holds D I V E (subPredAlphaAux c τ σ F) :=
+  Holds D I' V' E F ↔ Holds D I V E (subPredAlphaAux c τ σ S T F) :=
   by
   intro I'
-  induction F generalizing V V' V'' σ
+  induction F generalizing V V' σ
   case pred_const_ X xs =>
+    simp only [isFreeIn] at h1
+
     simp only [subPredAlphaAux]
     simp only [Holds]
     simp only [Interpretation.usingPred]
@@ -126,23 +129,16 @@ example
     simp only [List.map_eq_map_iff]
     intro x a1
     simp
-    simp only [isFreeIn] at h1
     exact h1 x a1
   case pred_var_ X xs =>
+    simp only [isFreeIn] at h1
+
     simp only [subPredAlphaAux]
     simp only [Holds]
     simp only [Interpretation.usingPred]
     simp
     split_ifs
     case _ c1 c2 =>
-      simp only [isFreeIn] at h1
-
-      simp only [predVarSet] at h2
-      simp at h2
-      simp only [predVarFreeVarSet] at h2
-      simp only [c1] at h2
-      simp at h2
-
       generalize (Option.get (τ X (List.length xs)) (_ : Option.isSome (τ X (List.length xs)) = true)).1 = zs at *
 
       generalize (Option.get (τ X (List.length xs)) (_ : Option.isSome (τ X (List.length xs)) = true)).2 = H at *
@@ -152,6 +148,7 @@ example
 
       simp at s1
       simp only [s1]
+      clear s1
 
       apply Holds_coincide_Var
       intro x a1
@@ -163,10 +160,7 @@ example
         · exact c3
       · simp only [Function.updateListITE_not_mem V'' x zs (List.map V' xs) c3]
         simp only [Function.updateListITE_not_mem V x zs (List.map (V ∘ σ ) xs) c3]
-        apply h2
-        · simp only [isFreeIn_iff_mem_freeVarSet] at a1
-          exact a1
-        · exact c3
+        sorry
     case _ c1 c2 =>
       simp only [Holds]
       simp
@@ -174,7 +168,6 @@ example
       simp only [List.map_eq_map_iff]
       intro x a1
       simp
-      simp only [isFreeIn] at h1
       exact h1 x a1
     case _ c1 =>
       simp only [Holds]
@@ -183,25 +176,21 @@ example
       simp only [List.map_eq_map_iff]
       intro x a1
       simp
-      simp only [isFreeIn] at h1
       exact h1 x a1
   case forall_ x phi ih =>
     simp only [isFreeIn] at h1
 
-    simp only [predVarSet] at h2
-    simp at h2
-    simp only [predVarFreeVarSet] at h2
-
     simp only [subPredAlphaAux]
     simp only [Interpretation.usingPred]
     simp only [Holds]
+
     apply forall_congr'
     intro d
+
     apply ih
     · intro v a1
       split_ifs
       case _ c1 =>
-        generalize (fresh x c (Finset.biUnion (predVarSet (subPredAlphaAux c τ (Function.updateITE σ x x) phi)) (predVarFreeVarSet τ))) = x' at *
         simp only [Function.updateITE]
         split_ifs
         case _ c2 c3 =>
@@ -209,18 +198,88 @@ example
         case _ c2 c3 =>
           contradiction
         case _ c2 c3 =>
-          specialize h1 v
-          simp only [c2] at h1
-          simp only [a1] at h1
-          simp at h1
-          subst c3
-          sorry
+          obtain s1 := fresh_not_mem x c (Finset.image (Function.updateITE σ x x) (freeVarSet phi))
+          simp only [← c3] at s1
+          obtain s2 := Sub.Var.All.Rec.freeVarSet_subFresh_eq_freeVarSet_image (Function.updateITE σ x x) c phi
+          simp only [s2] at c1
+
+          simp only [isFreeIn_iff_mem_freeVarSet] at a1
+          exfalso
+          apply s1
+          obtain s5 := Finset.mem_image_of_mem (Function.updateITE σ x x) a1
+          simp only [Function.updateITE] at s5
+          simp only [if_neg c2] at s5
+          exact s5
         case _ c2 c3 =>
           apply h1
           tauto
       case _ c1 =>
-        sorry
-    · sorry
+        simp only [Function.updateITE]
+        by_cases c2 : v = x
+        · simp only [if_pos c2]
+          simp
+        · simp only [if_neg c2]
+          split_ifs
+          case _ c3 =>
+            obtain s1 := Sub.Var.All.Rec.freeVarSet_subFresh_eq_freeVarSet_image (Function.updateITE σ x x) c phi
+            simp only [s1] at c1
+            exfalso
+            apply c1
+            simp only [← c3]
+            simp only [isFreeIn_iff_mem_freeVarSet] at a1
+            obtain s5 := Finset.mem_image_of_mem (Function.updateITE σ (σ v) (σ v)) a1
+            simp only [Function.updateITE] at s5
+            simp only [Finset.mem_union]
+            left
+
+            split_ifs at s5
+            · exact s5
+            · exact s5
+          case _ c3 =>
+            apply h1
+            tauto
+    · intro v a1
+      simp only [predVarSet] at h2
+      specialize h2 v a1
+      split_ifs
+      case _ c1 =>
+        simp only [Function.updateITE]
+        split_ifs
+        case _ c2 =>
+          simp only [Finset.mem_union] at c1
+          obtain s1 := Sub.Var.All.Rec.freeVarSet_subFresh_eq_freeVarSet_image (Function.updateITE σ x x) c phi
+          --simp only [s1] at c1
+          simp only [<- s1] at c2
+          obtain s30 := fresh_not_mem x c (freeVarSet (Var.All.Rec.subFresh (Function.updateITE σ x x) c phi))
+          simp only [← c2] at s30
+          cases c1
+          case _ c1 =>
+            have s50 : ¬ v = x
+            intro contra
+            subst contra
+            apply s30
+            exact c1
+
+            sorry
+          case _ c1 =>
+
+            sorry
+        case _ c2 =>
+          exact h2
+      case _ c1 =>
+        simp only [Finset.mem_union] at c1
+        push_neg at c1
+        cases c1
+        case _ c1_left c1_right =>
+          have s20 : ¬ v = x
+          intro contra
+          apply c1_right
+          subst contra
+          exact a1
+          simp only [Function.updateITE]
+          simp only [if_neg s20]
+          exact h2
+
 
   all_goals
     sorry
