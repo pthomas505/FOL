92f98d22d2b3f2600f91ebfc2f8cba763c980d95
Patrick Thomas <pthomas505@gmail.com>
2023-12-22 11:45:24 -0500

    Updates.

--- FOL/NV/Sub/Prop/All/Rec/Sub.lean
+++ FOL/NV/Sub/Prop/All/Rec/Sub.lean
@@ -14,7 +14,7 @@ open Formula
 /--
   The recursive simultaneous uniform substitution of all of the propositional variables in a formula.
 -/
-def replacePropFun (τ : PredName → PredName) : Formula → Formula
+def sub (τ : PredName → PredName) : Formula → Formula
   | pred_const_ P ts => pred_const_ P ts
   | pred_var_ P ts =>
       if ts = List.nil
@@ -23,13 +23,13 @@ def replacePropFun (τ : PredName → PredName) : Formula → Formula
   | eq_ x y => eq_ x y
   | true_ => true_
   | false_ => false_
-  | not_ phi => not_ (replacePropFun τ phi)
-  | imp_ phi psi => imp_ (replacePropFun τ phi) (replacePropFun τ psi)
-  | and_ phi psi => and_ (replacePropFun τ phi) (replacePropFun τ psi)
-  | or_ phi psi => or_ (replacePropFun τ phi) (replacePropFun τ psi)
-  | iff_ phi psi => iff_ (replacePropFun τ phi) (replacePropFun τ psi)
-  | forall_ x phi => forall_ x (replacePropFun τ phi)
-  | exists_ x phi => exists_ x (replacePropFun τ phi)
+  | not_ phi => not_ (sub τ phi)
+  | imp_ phi psi => imp_ (sub τ phi) (sub τ psi)
+  | and_ phi psi => and_ (sub τ phi) (sub τ psi)
+  | or_ phi psi => or_ (sub τ phi) (sub τ psi)
+  | iff_ phi psi => iff_ (sub τ phi) (sub τ psi)
+  | forall_ x phi => forall_ x (sub τ phi)
+  | exists_ x phi => exists_ x (sub τ phi)
   | def_ P ts => def_ P ts
 
 
@@ -44,30 +44,30 @@ instance {α : Type} {xs : List α} : Decidable (xs = []) :=
     exact instDecidableFalse
 
 
-lemma replacePropFun_no_predVar
+lemma sub_no_predVar
   (F : Formula)
   (τ : PredName → PredName)
   (h1 : F.predVarSet = ∅) :
-  replacePropFun τ F = F :=
+  sub τ F = F :=
   by
   induction F
   case pred_const_ X xs =>
-    unfold replacePropFun
+    unfold sub
     rfl
   case pred_var_ X xs =>
     unfold predVarSet at h1
 
     simp at h1
   case eq_ x y =>
-    unfold replacePropFun
+    unfold sub
     rfl
   case true_ | false_ =>
-    unfold replacePropFun
+    unfold sub
     rfl
   case not_ phi phi_ih =>
     unfold predVarSet at h1
 
-    unfold replacePropFun
+    unfold sub
     congr!
     exact phi_ih h1
   case
@@ -80,29 +80,29 @@ lemma replacePropFun_no_predVar
 
     cases h1
     case intro h1_left h1_right =>
-      unfold replacePropFun
+      unfold sub
       congr!
       · exact phi_ih h1_left
       · exact psi_ih h1_right
   case forall_ x phi phi_ih | exists_ x phi phi_ih =>
     unfold predVarSet at h1
 
-    unfold replacePropFun
+    unfold sub
     congr!
     exact phi_ih h1
   case def_ X xs =>
-    unfold replacePropFun
+    unfold sub
     rfl
 
 
-theorem prop_sub_aux
+theorem substitution_theorem
   (D : Type)
   (I : Interpretation D)
   (V : VarAssignment D)
   (E : Env)
   (τ : PredName → PredName)
   (F : Formula) :
-  Holds D I V E (replacePropFun τ F) ↔
+  Holds D I V E (sub τ F) ↔
     Holds D
       ⟨
         I.nonempty,
@@ -118,10 +118,10 @@ theorem prop_sub_aux
   all_goals
     induction F generalizing V
     case pred_const_ X xs =>
-      unfold replacePropFun
+      unfold sub
       simp only [Holds]
     case pred_var_ X xs =>
-        unfold replacePropFun
+        unfold sub
         split_ifs
         case pos c1 =>
           simp only [Holds]
@@ -134,13 +134,13 @@ theorem prop_sub_aux
           simp
           simp only [if_neg c1]
     case eq_ x y =>
-      unfold replacePropFun
+      unfold sub
       simp only [Holds]
     case true_ | false_ =>
-      unfold replacePropFun
+      unfold sub
       simp only [Holds]
     case not_ phi phi_ih =>
-      unfold replacePropFun
+      unfold sub
       simp only [Holds] at phi_ih
 
       simp only [Holds]
@@ -154,7 +154,7 @@ theorem prop_sub_aux
       simp only [Holds] at phi_ih
       simp only [Holds] at psi_ih
 
-      unfold replacePropFun
+      unfold sub
       simp only [Holds]
       congr! 1
       · apply phi_ih
@@ -162,43 +162,43 @@ theorem prop_sub_aux
     case forall_ x phi phi_ih | exists_ x phi phi_ih =>
       simp only [Holds] at phi_ih
 
-      unfold replacePropFun
+      unfold sub
       simp only [Holds]
       first | apply forall_congr' | apply exists_congr
       intros d
       apply phi_ih
 
   case nil.def_ X xs =>
-    unfold replacePropFun
+    unfold sub
     simp only [Holds]
   case cons.def_ hd tl ih X xs =>
     simp only [Holds] at ih
     simp at ih
 
-    unfold replacePropFun
+    unfold sub
     simp only [Holds]
     split_ifs
     case _ c1 =>
       specialize ih (Function.updateListITE V hd.args (List.map V xs)) hd.q
-      simp only [replacePropFun_no_predVar hd.q τ hd.h2] at ih
+      simp only [sub_no_predVar hd.q τ hd.h2] at ih
       apply ih
     case _ c1 =>
       specialize ih V (def_ X xs)
-      unfold replacePropFun at ih
+      unfold sub at ih
       exact ih
 
 
-theorem prop_sub_isValid
+theorem substitution_is_valid
   (phi : Formula)
   (h1 : phi.IsValid)
   (τ : PredName → PredName) :
-  (replacePropFun τ phi).IsValid :=
+  (sub τ phi).IsValid :=
   by
   unfold IsValid at h1
 
   unfold IsValid
   intro D I V E
-  simp only [prop_sub_aux D I V E τ phi]
+  simp only [substitution_theorem D I V E τ phi]
   apply h1
 
 
