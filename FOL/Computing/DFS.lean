e8c58e267a962a96ab54fb48c80822eabf8b5cfa
Patrick Thomas <pthomas505@gmail.com>
2024-05-14 21:21:06 -0700

    Updates.

--- FOL/Computing/DFS.lean
+++ FOL/Computing/DFS.lean
@@ -38,7 +38,7 @@ def nextss
   (g : Graph Node)
   (xs : List Node) :
   Set Node :=
-  {y | ∃ x, x ∈ xs ∧ (x, y) ∈ g}
+  {y | ∃ (x : Node), x ∈ xs ∧ (x, y) ∈ g}
 
 
 lemma nexts_set
@@ -47,31 +47,31 @@ lemma nexts_set
   (g : Graph Node) :
   ∀ (x y : Node), y ∈ nexts g x ↔ (x, y) ∈ g :=
   by
-  induction g
-  case nil =>
-    simp only [nexts]
-    simp
-  case cons hd tl ih =>
-    simp only [nexts]
-    intro x y
-    split
-    case inl c1 =>
-      subst c1
-      simp only [List.mem_cons]
-      simp only [ih]
-      constructor
-      · tauto
-      · intro a1
-        simp only [Prod.eq_iff_fst_eq_snd_eq] at a1
-        tauto
-    case inr c1 =>
-      simp only [List.mem_cons]
-      simp only [ih]
-      constructor
-      · tauto
-      · intro a1
-        simp only [Prod.eq_iff_fst_eq_snd_eq] at a1
-        tauto
+    induction g
+    case nil =>
+      simp only [nexts]
+      simp
+    case cons hd tl ih =>
+      simp only [nexts]
+      intro x y
+      split
+      case inl c1 =>
+        subst c1
+        simp only [List.mem_cons]
+        simp only [ih]
+        constructor
+        · tauto
+        · intro a1
+          simp only [Prod.eq_iff_fst_eq_snd_eq] at a1
+          tauto
+      case inr c1 =>
+        simp only [List.mem_cons]
+        simp only [ih]
+        constructor
+        · tauto
+        · intro a1
+          simp only [Prod.eq_iff_fst_eq_snd_eq] at a1
+          tauto
 
 
 lemma nextss_cons
@@ -107,35 +107,35 @@ lemma not_in_nodes_imp_nexts_empty
   (h1 : x ∉ g.nodes_of) :
   nexts g x = [] :=
   by
-  induction g
-  case nil =>
-    simp only [nexts]
-  case cons hd tl ih =>
-    simp only [Graph.nodes_of] at ih
-    simp only [List.mem_union_iff] at ih
-    push_neg at ih
+    induction g
+    case nil =>
+      simp only [nexts]
+    case cons hd tl ih =>
+      simp only [Graph.nodes_of] at ih
+      simp only [List.mem_union_iff] at ih
+      push_neg at ih
 
-    simp only [Graph.nodes_of] at h1
-    simp only [List.mem_union_iff] at h1
-    push_neg at h1
-    cases h1
-    case _ h1_left h1_right =>
-      simp only [List.map_cons, List.mem_cons] at h1_left
-      push_neg at h1_left
-      simp only [ne_eq] at h1_left
-      cases h1_left
-      case _ h1_left_left h1_left_right =>
-        simp only [List.map_cons, List.mem_cons] at h1_right
-        push_neg at h1_right
-        simp only [ne_eq] at h1_right
-        cases h1_right
-        case _ h1_right_left h1_right_right =>
-          simp only [nexts]
-          split
-          case _ c1 =>
-            tauto
-          case _ c1 =>
-            tauto
+      simp only [Graph.nodes_of] at h1
+      simp only [List.mem_union_iff] at h1
+      push_neg at h1
+      cases h1
+      case _ h1_left h1_right =>
+        simp only [List.map_cons, List.mem_cons] at h1_left
+        push_neg at h1_left
+        simp only [ne_eq] at h1_left
+        cases h1_left
+        case _ h1_left_left h1_left_right =>
+          simp only [List.map_cons, List.mem_cons] at h1_right
+          push_neg at h1_right
+          simp only [ne_eq] at h1_right
+          cases h1_right
+          case _ h1_right_left h1_right_right =>
+            simp only [nexts]
+            split
+            case _ c1 =>
+              tauto
+            case _ c1 =>
+              tauto
 
 
 lemma List.erase_diff_len_lt_diff_len
@@ -147,14 +147,14 @@ lemma List.erase_diff_len_lt_diff_len
   (h2 : x ∉ l2) :
   ((l1.erase x).diff l2).length < (l1.diff l2).length :=
   by
-  obtain s1 := List.mem_diff_of_mem h1 h2
-  obtain s2 := List.length_erase_of_mem s1
-  obtain s3 := List.length_pos_of_mem s1
-  obtain s4 := Nat.pred_lt' s3
-  simp only [← s2] at s4
-  obtain s5 := List.diff_erase l1 l2 x
-  simp only [← s5]
-  exact s4
+    obtain s1 := List.mem_diff_of_mem h1 h2
+    obtain s2 := List.length_erase_of_mem s1
+    obtain s3 := List.length_pos_of_mem s1
+    obtain s4 := Nat.pred_lt' s3
+    simp only [← s2] at s4
+    obtain s5 := List.diff_erase l1 l2 x
+    simp only [← s5]
+    exact s4
 
 
 /--
@@ -176,24 +176,24 @@ def dfs_aux
 
   termination_by ((g.nodes_of.diff visited).length, stack.length)
   decreasing_by
-  case _ _ =>
-    simp_wf
-    decreasing_trivial
-  case _ c1 =>
-    simp_wf
-    by_cases c2 : x ∈ g.nodes_of
-    case pos =>
-      obtain s1 := List.erase_diff_len_lt_diff_len g.nodes_of visited x c2 c1
-      exact Prod.Lex.left ((nexts g x).length + xs.length) (xs.length + 1) s1
-    case neg =>
-      have s1 : (g.nodes_of.erase x) = g.nodes_of := by
-        exact List.erase_of_not_mem c2
-      simp only [s1]
+    case _ _ =>
+      simp_wf
+      decreasing_trivial
+    case _ c1 =>
+      simp_wf
+      by_cases c2 : x ∈ g.nodes_of
+      case pos =>
+        obtain s1 := List.erase_diff_len_lt_diff_len g.nodes_of visited x c2 c1
+        exact Prod.Lex.left ((nexts g x).length + xs.length) (xs.length + 1) s1
+      case neg =>
+        have s1 : (g.nodes_of.erase x) = g.nodes_of := by
+          exact List.erase_of_not_mem c2
+        simp only [s1]
 
-      simp only [not_in_nodes_imp_nexts_empty g x c2]
-      simp
-      apply Prod.Lex.right (g.nodes_of.diff visited).length
-      exact Nat.lt.base xs.length
+        simp only [not_in_nodes_imp_nexts_empty g x c2]
+        simp
+        apply Prod.Lex.right (g.nodes_of.diff visited).length
+        exact Nat.lt.base xs.length
 
 
 /--
@@ -224,7 +224,8 @@ example
   (g : Graph Node)
   (visited : List Node) :
   dfs_aux g [] visited = visited :=
-  by simp only [dfs_aux]
+  by
+    simp only [dfs_aux]
 
 
 example
@@ -249,24 +250,24 @@ lemma dfs_app
   (zs : List Node) :
   dfs_aux g (xs ++ ys) zs = dfs_aux g ys (dfs_aux g xs zs) :=
   by
-  induction xs, zs using dfs_aux.induct g
-  case _ visited =>
-    simp only [dfs_aux]
-    simp
-  case _ visited x xs c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_pos c1]
-    simp only [← ih]
-    simp
-    simp only [dfs_aux]
-    simp only [if_pos c1]
-  case _ visited x xs c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_neg c1]
-    simp only [← ih]
-    simp
-    simp only [dfs_aux]
-    simp only [if_neg c1]
+    induction xs, zs using dfs_aux.induct g
+    case _ visited =>
+      simp only [dfs_aux]
+      simp
+    case _ visited x xs c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_pos c1]
+      simp only [← ih]
+      simp
+      simp only [dfs_aux]
+      simp only [if_pos c1]
+    case _ visited x xs c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_neg c1]
+      simp only [← ih]
+      simp
+      simp only [dfs_aux]
+      simp only [if_neg c1]
 
 
 lemma visit_subset_dfs
@@ -277,20 +278,20 @@ lemma visit_subset_dfs
   (visited : List Node) :
   visited ⊆ dfs_aux g stack visited :=
   by
-  induction stack, visited using dfs_aux.induct g
-  case _ visited =>
-    simp only [dfs_aux]
-    simp
-  case _ visited x stack c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_pos c1]
-    exact ih
-  case _ visited x stack c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_neg c1]
-    trans (x :: visited)
-    · simp
-    · exact ih
+    induction stack, visited using dfs_aux.induct g
+    case _ visited =>
+      simp only [dfs_aux]
+      simp
+    case _ visited x stack c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_pos c1]
+      exact ih
+    case _ visited x stack c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_neg c1]
+      trans (x :: visited)
+      · simp
+      · exact ih
 
 
 lemma next_subset_dfs
@@ -301,31 +302,31 @@ lemma next_subset_dfs
   (visited : List Node) :
   stack ⊆ dfs_aux g stack visited :=
   by
-  induction stack, visited using dfs_aux.induct g
-  case _ visited =>
-    simp only [dfs_aux]
-    simp
-  case _ visited x stack c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_pos c1]
-    simp
-    constructor
-    case left =>
-      obtain s1 := visit_subset_dfs g stack visited
-      apply Set.mem_of_subset_of_mem s1 c1
-    case right =>
-      exact ih
-  case _ visited x stack c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_neg c1]
-    simp
-    constructor
-    · have s2 : x ∈ x :: visited := by { simp }
-      obtain s1 := visit_subset_dfs g (nexts g x ++ stack) (x :: visited)
-      apply Set.mem_of_subset_of_mem s1 s2
-    · trans (nexts g x) ++ stack
-      · simp
-      · exact ih
+    induction stack, visited using dfs_aux.induct g
+    case _ visited =>
+      simp only [dfs_aux]
+      simp
+    case _ visited x stack c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_pos c1]
+      simp
+      constructor
+      case left =>
+        obtain s1 := visit_subset_dfs g stack visited
+        apply Set.mem_of_subset_of_mem s1 c1
+      case right =>
+        exact ih
+    case _ visited x stack c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_neg c1]
+      simp
+      constructor
+      · have s2 : x ∈ x :: visited := by { simp }
+        obtain s1 := visit_subset_dfs g (nexts g x ++ stack) (x :: visited)
+        apply Set.mem_of_subset_of_mem s1 s2
+      · trans (nexts g x) ++ stack
+        · simp
+        · exact ih
 
 
 lemma nextss_closed_dfs'
@@ -337,50 +338,50 @@ lemma nextss_closed_dfs'
   (h1 : nextss g visited ⊆ stack.toFinset ∪ visited.toFinset.toSet) :
   nextss g (dfs_aux g stack visited) ⊆ (dfs_aux g stack visited).toFinset.toSet :=
   by
-  induction stack, visited using dfs_aux.induct g
-  case _ visited =>
-    simp at h1
-    simp only [dfs_aux]
-    simp
-    exact h1
-  case _ visited x stack c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_pos c1]
-    apply ih
-    trans ↑(x :: stack).toFinset ∪ ↑visited.toFinset
-    · exact h1
-    · simp only [List.toFinset_cons, Finset.coe_insert]
-      simp only [Set.insert_union]
-
-      have s1 : x ∈ (stack.toFinset.toSet ∪ visited.toFinset.toSet) :=
-      by { simp; right; exact c1 }
-
-      obtain s2 := Set.insert_eq_of_mem s1
-      simp only [s2]
-      simp
-  case _ visited x stack c1 ih =>
-    simp only [dfs_aux]
-    simp only [if_neg c1]
-    apply ih
-    simp only [nextss_cons]
-    simp only [Set.union_subset_iff]
-    constructor
-    · simp only [List.toFinset_append, Finset.coe_union]
-      apply Set.subset_union_of_subset_left
-      apply Set.subset_union_left
-    · simp only [List.toFinset_cons] at h1
+    induction stack, visited using dfs_aux.induct g
+    case _ visited =>
       simp at h1
-
-      simp only [List.toFinset_cons]
-      simp only [List.toFinset_append, Finset.coe_union, List.toFinset_cons, Set.union_insert]
-      simp only [Set.union_assoc]
-      apply Set.subset_union_of_subset_right
-
-      have s1 : (insert x stack.toFinset.toSet) ∪ visited.toFinset.toSet = stack.toFinset.toSet ∪ (insert x visited.toFinset).toSet := by aesop
-      simp only [<- s1]
-
+      simp only [dfs_aux]
       simp
       exact h1
+    case _ visited x stack c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_pos c1]
+      apply ih
+      trans ↑(x :: stack).toFinset ∪ ↑visited.toFinset
+      · exact h1
+      · simp only [List.toFinset_cons, Finset.coe_insert]
+        simp only [Set.insert_union]
+
+        have s1 : x ∈ (stack.toFinset.toSet ∪ visited.toFinset.toSet) :=
+        by { simp; right; exact c1 }
+
+        obtain s2 := Set.insert_eq_of_mem s1
+        simp only [s2]
+        simp
+    case _ visited x stack c1 ih =>
+      simp only [dfs_aux]
+      simp only [if_neg c1]
+      apply ih
+      simp only [nextss_cons]
+      simp only [Set.union_subset_iff]
+      constructor
+      · simp only [List.toFinset_append, Finset.coe_union]
+        apply Set.subset_union_of_subset_left
+        apply Set.subset_union_left
+      · simp only [List.toFinset_cons] at h1
+        simp at h1
+
+        simp only [List.toFinset_cons]
+        simp only [List.toFinset_append, Finset.coe_union, List.toFinset_cons, Set.union_insert]
+        simp only [Set.union_assoc]
+        apply Set.subset_union_of_subset_right
+
+        have s1 : (insert x stack.toFinset.toSet) ∪ visited.toFinset.toSet = stack.toFinset.toSet ∪ (insert x visited.toFinset).toSet := by aesop
+        simp only [<- s1]
+
+        simp
+        exact h1
 
 
 lemma nextss_closed_dfs
@@ -507,4 +508,4 @@ lemma reachable_closed_dfs
 
 
 
-#lint
+--#lint
