b541bee0f96d23d0d67a84e39b06f26b15ab05bf
Patrick Thomas <pthomas505@gmail.com>
2024-05-19 20:31:37 -0700

    Updates.

--- FOL/Computing/NFA.lean
+++ FOL/Computing/NFA.lean
@@ -194,3 +194,71 @@ example : symbol_step_multiple_list_to_fun [⟨0, 'a', [1]⟩, ⟨0, 'b', [2]⟩
 example : symbol_step_multiple_list_to_fun [⟨0, 'a', [1]⟩, ⟨0, 'b', [2]⟩] 0 'b' == [2] := by rfl
 
 example : symbol_step_multiple_list_to_fun [⟨0, 'a', [1]⟩, ⟨0, 'a', [2]⟩] 0 'a' == [1, 2] := by rfl
+
+
+/--
+  `NFA.evalOne e l c` := Returns the list of states that the nondeterministic automaton `e` transitions to if it starts at the list of states `l` and consumes the symbol `c`.
+-/
+def NFA.evalOne
+  {α : Type}
+  [DecidableEq α]
+  {σ : Type}
+  [DecidableEq σ]
+  (e : NFA α σ)
+  (state_list : List σ)
+  (symbol : α) :
+  List σ :=
+  (state_list.map (fun (state : σ) => symbol_step_multiple_list_to_fun e.symbol_step_list state symbol)).join
+
+
+def NFA.evalFrom
+  {α : Type}
+  [DecidableEq α]
+  {σ : Type}
+  [DecidableEq σ]
+  (e : NFA α σ)
+  (startingStateList : List σ) :
+  List α → List σ :=
+  List.foldl e.evalOne startingStateList
+
+
+/--
+  `NFA.eval e cs` := Returns the final list of states that the nondeterministic automaton `e` transitions to if it starts at `e.starting_state_list` and consumes the list of symbols `cs`.
+-/
+def NFA.eval
+  {α : Type}
+  [DecidableEq α]
+  {σ : Type}
+  [DecidableEq σ]
+  (e : NFA α σ) :
+  List α → List σ :=
+  e.evalFrom e.starting_state_list
+
+
+#eval NFA.eval ⟨ [⟨0, 'a', [1]⟩], [0], [1] ⟩ ['a']
+
+
+def NFA.accepts
+  {α : Type}
+  [DecidableEq α]
+  {σ : Type}
+  [DecidableEq σ]
+  (e : NFA α σ)
+  (input : List α) :
+  Prop :=
+  ∃ (s : σ), s ∈ e.eval input ∧ s ∈ e.accepting_state_list
+
+
+instance
+  {α : Type}
+  [DecidableEq α]
+  {σ : Type}
+  [DecidableEq σ]
+  (e : NFA α σ)
+  (input : List α) :
+  Decidable (e.accepts input) :=
+  by
+  induction input
+  all_goals
+    simp only [NFA.accepts]
+    infer_instance
