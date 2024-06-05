600ad9d194ef363d31b1f8b5c416cf8534d1c754
Patrick Thomas <pthomas505@gmail.com>
2024-06-04 23:50:17 -0700

    Updates.

--- FOL/Computing/NFA.lean
+++ FOL/Computing/NFA.lean
@@ -6,11 +6,9 @@ set_option autoImplicit false
 
 structure NFA
   (α : Type)
-  [DecidableEq α]
-  (σ : Type)
-  [DecidableEq σ] :
+  (σ : Type) :
   Type :=
-  (symbol_step_list : List (SymbolStepMultiple α σ))
+  (symbol_arrow_list : List (SymbolArrow α σ))
   (starting_state_list : List σ)
   (accepting_state_list : List σ)
   deriving Repr
@@ -19,6 +17,7 @@ structure NFA
 /--
   `NFA.eval_one e l c` := Returns the list of states that the nondeterministic automaton `e` transitions to if it starts at the list of states `l` and consumes the symbol `c`.
 -/
+@[simp]
 def NFA.eval_one
   {α : Type}
   [DecidableEq α]
@@ -28,31 +27,36 @@ def NFA.eval_one
   (state_list : List σ)
   (symbol : α) :
   List σ :=
-  (state_list.map (fun (state : σ) => symbol_step_multiple_list_to_fun e.symbol_step_list state symbol)).join
+  (state_list.map (fun (state : σ) => (symbol_arrow_list_to_fun e.symbol_arrow_list) state symbol)).join.dedup
 
 
+
+@[simp]
 def NFA.eval_from
   {α : Type}
   [DecidableEq α]
   {σ : Type}
   [DecidableEq σ]
   (e : NFA α σ)
-  (starting_state_list : List σ) :
-  List α → List σ :=
-  List.foldl e.eval_one starting_state_list
+  (starting_state_list : List σ)
+  (input : List α) :
+  List σ :=
+  List.foldl e.eval_one starting_state_list input
 
 
 /--
   `NFA.eval e cs` := Returns the final list of states that the nondeterministic automaton `e` transitions to if it starts at `e.starting_state_list` and consumes the list of symbols `cs`.
 -/
+@[simp]
 def NFA.eval
   {α : Type}
   [DecidableEq α]
   {σ : Type}
   [DecidableEq σ]
-  (e : NFA α σ) :
-  List α → List σ :=
-  e.eval_from e.starting_state_list
+  (e : NFA α σ)
+  (input : List α) :
+  List σ :=
+  e.eval_from e.starting_state_list input
 
 
 example : NFA.eval ⟨ [], [0], [1] ⟩ ['a'] = [] := by rfl
@@ -72,6 +76,7 @@ example : NFA.eval ⟨ [⟨0, 'a', [1]⟩, ⟨0, 'b', [2]⟩], [0], [1] ⟩ ['b'
 example : NFA.eval ⟨ [⟨0, 'a', [1]⟩, ⟨0, 'a', [2]⟩], [0], [1] ⟩ ['a'] = [1, 2] := by rfl
 
 
+@[simp]
 def NFA.accepts
   {α : Type}
   [DecidableEq α]
