ac975430a4c8def57150d24e7ca0f80c23777007
Patrick Thomas <pthomas505@gmail.com>
2024-06-07 23:12:37 -0700

    Updates.

--- FOL/Parsing/Brzozowski.lean
+++ FOL/Parsing/Brzozowski.lean
@@ -1,24 +1,61 @@
+import FOL.Parsing.DFA
 import FOL.Parsing.RegExp
 
 
 set_option autoImplicit false
 
 
--- Take the set of strings in S that start with u and then remove the u from them.
+/-
+  https://people.cs.uchicago.edu/~jhr/papers/2009/jfp-re-derivatives.pdf
+-/
+
+/-
+We assume a finite alphabet Σ of symbols and use Σ∗ to denote the set of all finite strings over Σ. We use a, b, c, etc. to represent symbols and u, v, w to represent strings. The empty string is denoted by ε. A language of Σ is a (possibly infinite) set of finite strings L ⊆ Σ∗.
+-/
+
+/-
+  This formalization uses α for Σ since Σ is reserved in Lean.
+-/
+
+-- Str α is Σ∗.
+abbrev Str (α : Type) : Type := List α
+
+-- Language α is L.
+abbrev Language (α : Type) : Type := Set (Str α)
+
 
 /--
-  The Brzozowski derivative u^{-1}S of a set S of strings and a string u is the set of all strings obtainable from a string in S by cutting off the prefix u.
+Definition 3.1
+The derivative of a language L ⊆ Σ∗ with respect to a string u ∈ Σ∗ is defined to be ∂u L = {v | u · v ∈ L}.
 -/
-def Brzozowski_derivative
-  (S : Set (String))
-  (u : String) :
-  Set String :=
-  { v : String | u ++ v ∈ S }
+def derivative
+  {α : Type}
+  (S : Set (Str α))
+  (u : Str α) :
+  Set (Str α) :=
+  { v : Str α | u ++ v ∈ S }
+
+
+/--
+  The regular languages are those languages that can be described by regular expressions.
+-/
+def Language.isRegular
+  {α : Type}
+  (L : Language α) :
+  Prop :=
+  ∃ (e : RegExp α), L = e.languageOf
 
 
 /-
-The derivative, D_a(r) of a regular expression r by alphabet letter a is defined as D_a(r) = {w : aw ∈ r}. The derivative represents the set of continuations after letter a that the regular expression r can match.
+We say that an RE r is nullable if the language it defines contains the
+empty string, that is if ε ∈ L[[r]].
 -/
+
+def RegExp.nullable
+  {α : Type}
+  (e : RegExp α) :
+  Prop :=
+  [] ∈ e.languageOf
 
 
 def RegExp.delta
@@ -32,7 +69,64 @@ def RegExp.delta
   | closure _ => RegExp.epsilon
 
 
-def RegExp.Brzozowski_derivative_set
+example
+  (α : Type)
+  (R : RegExp α)
+  (cs : List α) :
+  cs ∈ R.delta.languageOf ↔ R.nullable ∧ cs = [] :=
+  by
+    induction R generalizing cs
+    case char a =>
+      simp only [RegExp.delta]
+      simp only [RegExp.nullable]
+      simp only [RegExp.languageOf]
+      simp
+    case epsilon =>
+      simp only [RegExp.delta]
+      simp only [RegExp.nullable]
+      simp only [RegExp.languageOf]
+      simp
+    case zero =>
+      simp only [RegExp.delta]
+      simp only [RegExp.nullable]
+      simp only [RegExp.languageOf]
+      simp
+    case union r s r_ih s_ih =>
+      simp only [RegExp.nullable] at r_ih
+      simp only [RegExp.nullable] at s_ih
+
+      simp only [RegExp.delta]
+      simp only [RegExp.nullable]
+      simp only [RegExp.languageOf]
+      simp
+
+      specialize r_ih cs
+      specialize s_ih cs
+      tauto
+    case concat r s r_ih s_ih =>
+      simp only [RegExp.nullable] at r_ih
+      simp only [RegExp.nullable] at s_ih
+
+      simp only [RegExp.delta]
+      simp only [RegExp.nullable]
+      simp only [RegExp.languageOf]
+
+      aesop
+    case closure r _ =>
+      simp only [RegExp.delta]
+      simp only [RegExp.nullable]
+      simp only [RegExp.languageOf]
+
+      simp
+      intro _
+      apply Exists.intro []
+      simp
+
+
+/-
+The derivative, D_a(r) of a regular expression r by alphabet letter a is defined as D_a(r) = {w : aw ∈ r}. The derivative represents the set of continuations after letter a that the regular expression r can match.
+-/
+def RegExp.derivative_set
   {α : Type}
   [DecidableEq α]
   (r : RegExp α)
@@ -41,17 +135,122 @@ def RegExp.Brzozowski_derivative_set
   {w : List α | (a :: w) ∈ r.languageOf}
 
 
-def RegExp.Brzozowski_derivative
+def RegExp.derivative
   {α : Type}
   [DecidableEq α]
   (a : α) :
   RegExp α → RegExp α
-  | char b => if a = b then RegExp.epsilon else RegExp.zero
+  | char b =>
+      if a = b
+      then RegExp.epsilon
+      else RegExp.zero
+
   | epsilon => RegExp.zero
+
   | zero => RegExp.zero
-  | union r s => RegExp.union (r.Brzozowski_derivative a) (s.Brzozowski_derivative a)
-  | concat r s => RegExp.union (RegExp.concat (r.Brzozowski_derivative a) s) (RegExp.concat r.delta (s.Brzozowski_derivative a))
-  | closure r => RegExp.concat (r.Brzozowski_derivative a) r.closure
+
+  | union r s => RegExp.union (r.derivative a) (s.derivative a)
+
+  | concat r s =>
+    RegExp.union
+      (RegExp.concat (r.derivative a) s)
+      (RegExp.concat r.delta (s.derivative a))
+
+  | closure r => RegExp.concat (r.derivative a) r.closure
+
+
+lemma lem_3_1
+  {α : Type}
+  [DecidableEq α]
+  (L : Language α)
+  (h1 : Language.isRegular L)
+  (a : α) :
+  Language.isRegular (derivative L [a]) :=
+  by
+    simp only [Language.isRegular] at h1
+    apply Exists.elim h1
+    intro r a1
+    clear h1
+    subst a1
+    simp only [Language.isRegular]
+    simp only [derivative]
+    simp
+    apply Exists.intro (r.derivative a)
+    ext cs
+    simp
+
+    induction r
+    case h.char b =>
+      simp only [RegExp.derivative]
+      simp only [RegExp.languageOf]
+      simp
+      split_ifs
+      case pos c1 =>
+        tauto
+      case neg c1 =>
+        tauto
+    case h.epsilon =>
+      simp only [RegExp.derivative]
+      simp only [RegExp.languageOf]
+      simp
+    case h.zero =>
+      simp only [RegExp.derivative]
+      simp only [RegExp.languageOf]
+      simp
+    case h.union r s r_ih s_ih =>
+      simp only [RegExp.derivative]
+      simp only [RegExp.languageOf]
+      simp
+      tauto
+    case concat r s r_ih s_ih =>
+      simp only [RegExp.derivative]
+      simp only [RegExp.languageOf]
+      simp
+      constructor
+      · intro a1
+        apply Exists.elim a1
+        intro xs a2
+        clear a1
+        cases a2
+        case _ a2_left a2_right =>
+          apply Exists.elim a2_right
+          intro ys a3
+          clear a2_right
+          cases a3
+          case _ a3_left a3_right =>
+            simp only [← a3_right] at r_ih
+            simp only [← a3_right] at s_ih
+            by_cases c1 : (∃ r_1 ∈ RegExp.languageOf α (RegExp.derivative a r), ∃ s_1 ∈ RegExp.languageOf α s, r_1 ++ s_1 = cs)
+            case pos =>
+              left
+              exact c1
+            case neg =>
+              right
+              simp at c1
+              sorry
+      · sorry
+    case h.closure r ih =>
+      simp only [RegExp.derivative]
+      simp only [RegExp.languageOf]
+      simp
+      sorry
+
+
+
+
+/-
+Theorem 3.1
+If L ⊆ Σ∗ is regular, then ∂u L is regular for all strings u ∈ Σ∗.
+-/
+theorem thm_3_1
+  {α : Type}
+  (L : Language α)
+  (h1 : Language.isRegular L) :
+  ∀ (u : Str α), Language.isRegular (derivative L u) :=
+  by
+    sorry
+
+
 
 
 example
@@ -59,13 +258,13 @@ example
   [DecidableEq α]
   (r : RegExp α)
   (a : α) :
-  RegExp.Brzozowski_derivative_set r a = (r.Brzozowski_derivative a).languageOf :=
+  RegExp.derivative_set r a = (r.derivative a).languageOf :=
   by
   ext cs
   induction r
   case char a b =>
-    simp only [RegExp.Brzozowski_derivative_set]
-    simp only [RegExp.Brzozowski_derivative]
+    simp only [RegExp.derivative_set]
+    simp only [RegExp.derivative]
     split_ifs
     case pos c1 =>
       simp only [RegExp.languageOf]
@@ -78,13 +277,13 @@ example
       intro contra
       contradiction
   case epsilon =>
-    simp only [RegExp.Brzozowski_derivative_set]
-    simp only [RegExp.Brzozowski_derivative]
+    simp only [RegExp.derivative_set]
+    simp only [RegExp.derivative]
     simp only [RegExp.languageOf]
     simp
   case zero =>
-    simp only [RegExp.Brzozowski_derivative_set]
-    simp only [RegExp.Brzozowski_derivative]
+    simp only [RegExp.derivative_set]
+    simp only [RegExp.derivative]
     simp only [RegExp.languageOf]
     simp
   case union r s r_ih s_ih =>
@@ -92,20 +291,20 @@ example
     simp
     simp only [← r_ih]
     simp only [← s_ih]
-    simp only [RegExp.Brzozowski_derivative_set]
+    simp only [RegExp.derivative_set]
     simp only [RegExp.languageOf]
     simp
   case concat r s r_ih s_ih =>
-    simp only [RegExp.Brzozowski_derivative_set] at r_ih
+    simp only [RegExp.derivative_set] at r_ih
     simp at r_ih
 
-    simp only [RegExp.Brzozowski_derivative_set] at s_ih
+    simp only [RegExp.derivative_set] at s_ih
     simp at s_ih
 
-    simp only [RegExp.Brzozowski_derivative_set]
+    simp only [RegExp.derivative_set]
     simp
 
-    simp only [RegExp.Brzozowski_derivative]
+    simp only [RegExp.derivative]
 
     simp only [RegExp.languageOf]
     simp
@@ -140,11 +339,11 @@ example
       case _ a1_right =>
         sorry
   case closure r r_ih =>
-    simp only [RegExp.Brzozowski_derivative_set] at r_ih
+    simp only [RegExp.derivative_set] at r_ih
     simp at r_ih
 
-    simp only [RegExp.Brzozowski_derivative_set]
-    simp only [RegExp.Brzozowski_derivative]
+    simp only [RegExp.derivative_set]
+    simp only [RegExp.derivative]
     simp only [RegExp.languageOf]
     simp
     constructor
