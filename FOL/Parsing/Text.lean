cb870264a1a473f9e29e6fd7e227a72d866024bf
Patrick Thomas <pthomas505@gmail.com>
2024-06-08 23:18:55 -0700

    Updates.

--- FOL/Parsing/Text.lean
+++ FOL/Parsing/Text.lean
@@ -49,7 +49,7 @@ example : ['a', 'b'] ∈ exp Char 2 :=
 
 
 lemma rev_str_mem_exp
-  (α : Type)
+  {α : Type}
   (s : Str α) :
   ∃ (n : ℕ), s.reverse ∈ exp α n :=
   by
@@ -66,11 +66,11 @@ lemma rev_str_mem_exp
 
 
 lemma str_mem_exp
-  (α : Type)
+  {α : Type}
   (s : Str α) :
   ∃ (n : ℕ), s ∈ exp α n :=
   by
-    obtain s1 := rev_str_mem_exp α s.reverse
+    obtain s1 := rev_str_mem_exp s.reverse
     simp only [List.reverse_reverse] at s1
     exact s1
 
@@ -86,7 +86,7 @@ def kleene_closure
 
 
 lemma rev_str_mem_exp_str_len
-  (α : Type)
+  {α : Type}
   (s : Str α) :
   s.reverse ∈ exp α s.length :=
   by
@@ -100,10 +100,54 @@ lemma rev_str_mem_exp_str_len
 
 
 lemma str_mem_exp_str_len
-  (α : Type)
+  {α : Type}
   (s : Str α) :
   s ∈ exp α s.length :=
   by
-    obtain s1 := rev_str_mem_exp_str_len α s.reverse
+    obtain s1 := rev_str_mem_exp_str_len s.reverse
     simp at s1
     exact s1
+
+
+lemma mem_exp_imp_str_len_eq
+  {α : Type}
+  (s : Str α)
+  (n : ℕ)
+  (h1 : s ∈ exp α n) :
+  s.length = n :=
+  by
+    induction h1
+    case zero =>
+      simp
+    case succ m a s ih_1 ih_2 =>
+      simp
+      exact ih_2
+
+
+example
+  (α : Type)
+  (s t : Str α)
+  (h1 : s ∈ kleene_closure α)
+  (h2 : t ∈ kleene_closure α) :
+  s ++ t ∈ kleene_closure α :=
+  by
+    simp only [kleene_closure] at *
+    simp at *
+
+    apply Exists.elim h1
+    intro m a1
+    clear h1
+
+    apply Exists.elim h2
+    intro n a2
+    clear h2
+
+    apply Exists.intro (m + n)
+
+    obtain s1 := mem_exp_imp_str_len_eq s m a1
+    obtain s2 := mem_exp_imp_str_len_eq t n a2
+
+    obtain s3 := str_mem_exp_str_len (s ++ t)
+    simp at s3
+    simp only [s1, s2] at s3
+    exact s3
