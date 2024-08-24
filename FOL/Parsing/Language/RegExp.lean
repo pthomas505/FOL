a4e502f6ebfa191d56a4c4e98cf20a5ef2ffc116
Patrick Thomas <pthomas505@gmail.com>
2024-08-23 23:53:05 -0700

    Updates.

--- FOL/Parsing/Language/RegExp.lean
+++ FOL/Parsing/Language/RegExp.lean
@@ -61,15 +61,174 @@ example
       exact Language.IsRegLang.kleene_closure R.LanguageOf R_ih
 
 
-def derivative
+def RegExp.nullify
+  {α : Type} :
+  RegExp α → RegExp α
+  | char _ => zero
+  | epsilon => epsilon
+  | zero => zero
+  | union R S => union R.nullify S.nullify
+  | concat R S => concat R.nullify S.nullify
+  | kleene_closure _ => epsilon
+
+
+example
   {α : Type}
-  (R : RegExp α)
-  (s : Str α) :
-  Language.Language α :=
-  Language.derivative R.LanguageOf s
+  (RE : RegExp α) :
+  open Classical in
+  if [] ∈ RE.LanguageOf
+  then RE.nullify = RegExp.epsilon
+  else RE.nullify = RegExp.zero :=
+  by
+    induction RE
+    case char c =>
+      simp only [RegExp.nullify]
+      simp only [RegExp.LanguageOf]
+      simp
+    case epsilon =>
+      simp only [RegExp.nullify]
+      simp only [RegExp.LanguageOf]
+      simp
+    case zero =>
+      simp only [RegExp.nullify]
+      simp only [RegExp.LanguageOf]
+      simp
+    case union R S R_ih S_ih =>
+      sorry
+    all_goals
+      sorry
 
 
-def equivalent
+example
+  {α : Type}
+  [DecidableEq α]
+  (R : RegExp α) :
+  R.nullify.LanguageOf = Language.Language.nullify R.LanguageOf :=
+  by
+    induction R
+    case char c =>
+      simp only [RegExp.LanguageOf]
+      simp only [Language.Language.nullify]
+      simp
+    case epsilon =>
+      simp only [RegExp.LanguageOf]
+      simp only [Language.Language.nullify]
+      simp
+    case zero =>
+      simp only [RegExp.LanguageOf]
+      simp only [Language.Language.nullify]
+      simp
+    case union R S R_ih S_ih =>
+      simp only [RegExp.LanguageOf]
+      rw [R_ih]
+      rw [S_ih]
+      simp only [Language.Language.nullify]
+      ext cs
+      simp
+      tauto
+    case concat R S R_ih S_ih =>
+      simp only [RegExp.LanguageOf]
+      rw [R_ih]
+      rw [S_ih]
+      simp only [Language.Language.nullify]
+      ext cs
+      simp
+      split_ifs
+      case pos c1 c2 =>
+        simp only [Language.concat]
+        simp
+        simp only [c1]
+        simp only [c2]
+        simp
+      case neg c1 c2 =>
+        simp only [Language.concat]
+        simp
+        simp only [c1]
+        simp only [c2]
+        simp
+      case pos c1 c2 =>
+        simp only [Language.concat]
+        simp
+        simp only [c1]
+        simp only [c2]
+        simp
+      case neg c1 c2 =>
+        simp only [Language.concat]
+        simp
+        simp only [c1]
+        simp only [c2]
+        simp
+    case kleene_closure R _ =>
+      simp only [RegExp.LanguageOf]
+      simp only [Language.Language.nullify]
+      split_ifs
+      case pos _ =>
+        rfl
+      case neg c1 =>
+        exfalso
+        apply c1
+        exact Language.eps_mem_kleene_closure (RegExp.LanguageOf α R)
+
+
+def RegExp.derivative
+  {α : Type}
+  [DecidableEq α]
+  (a : α) :
+  RegExp α → RegExp α
+  | char b => if a = b then epsilon else zero
+  | epsilon => zero
+  | zero => zero
+  | union R S => union (R.derivative a) (S.derivative a)
+  | concat R S => union (concat (R.derivative a) S) (concat R.nullify (S.derivative a))
+  | kleene_closure R => concat (R.derivative a) (kleene_closure R)
+
+
+example
+  {α : Type}
+  [DecidableEq α]
+  (a : α)
+  (R : RegExp α) :
+  (R.derivative a).LanguageOf = Language.derivative R.LanguageOf [a] :=
+  by
+    induction R
+    case char c =>
+      simp only [RegExp.derivative]
+      simp only [Language.derivative]
+      split_ifs
+      case pos c1 =>
+        rw [c1]
+        simp only [RegExp.LanguageOf]
+        simp
+      case neg c1 =>
+        simp only [RegExp.LanguageOf]
+        simp
+        simp only [c1]
+        simp
+    case epsilon =>
+      simp only [RegExp.LanguageOf]
+      simp only [Language.derivative]
+      simp
+    case zero =>
+      simp only [RegExp.LanguageOf]
+      simp only [Language.derivative]
+      simp
+    case union R S R_ih S_ih =>
+      simp only [RegExp.LanguageOf]
+      rw [R_ih]
+      rw [S_ih]
+      simp only [Language.derivative]
+      ext cs
+      simp
+    case concat R S R_ih S_ih =>
+      simp only [RegExp.LanguageOf]
+      rw [R_ih]
+      rw [S_ih]
+      sorry
+    case kleene_closure R R_ih =>
+      sorry
+
+
+def equal
   (α : Type)
   (R S : RegExp α) :
   Prop :=
