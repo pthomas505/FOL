182c9101977aa7addd3327ce23301ea0b7bc4aa6
Patrick Thomas <pthomas505@gmail.com>
2024-09-08 18:16:07 -0700

    Updates.

--- FOL/Parsing/Language/RegExp.lean
+++ FOL/Parsing/Language/RegExp.lean
@@ -661,7 +661,7 @@ def concat_finset_regexp_regexp
   else ∅
 
 
-def RegExp.partial_derivative
+def RegExp.partial_derivative_wrt_char
   {α : Type}
   [DecidableEq α]
   (RE : RegExp α)
@@ -671,30 +671,30 @@ def RegExp.partial_derivative
   | char b => if a = b then {epsilon} else ∅
   | epsilon => ∅
   | zero => ∅
-  | union α β => (α.partial_derivative a) ∪ (β.partial_derivative a)
+  | union α β => (α.partial_derivative_wrt_char a) ∪ (β.partial_derivative_wrt_char a)
   | concat α β =>
       if α.is_nullable
-      then (concat_finset_regexp_regexp (α.partial_derivative a) β) ∪ (β.partial_derivative a)
-      else (concat_finset_regexp_regexp (α.partial_derivative a) β)
-  | kleene_closure α => concat_finset_regexp_regexp (α.partial_derivative a) (RegExp.kleene_closure α)
+      then (concat_finset_regexp_regexp (α.partial_derivative_wrt_char a) β) ∪ (β.partial_derivative_wrt_char a)
+      else (concat_finset_regexp_regexp (α.partial_derivative_wrt_char a) β)
+  | kleene_closure α => concat_finset_regexp_regexp (α.partial_derivative_wrt_char a) (RegExp.kleene_closure α)
 
 
-def RegExp.partial_derivative_of_finset
+def RegExp.partial_derivative_of_finset_wrt_char
   {α : Type}
   [DecidableEq α]
   (Γ : Finset (RegExp α))
   (a : α) :
   Finset (RegExp α) :=
-  Finset.biUnion Γ (fun (R : RegExp α) => partial_derivative R a)
+  Finset.biUnion Γ (fun (R : RegExp α) => partial_derivative_wrt_char R a)
 
 
-def RegExp.partial_derivative_wrt_str_aux
+def RegExp.partial_derivative_of_finset_wrt_str_aux
   {α : Type}
   [DecidableEq α]
   (Γ : Finset (RegExp α)) :
   Str α → Finset (RegExp α)
   | [] => Γ
-  | hd :: tl => RegExp.partial_derivative_wrt_str_aux (RegExp.partial_derivative_of_finset Γ hd) tl
+  | hd :: tl => RegExp.partial_derivative_of_finset_wrt_str_aux (RegExp.partial_derivative_of_finset_wrt_char Γ hd) tl
 
 
 def RegExp.partial_derivative_wrt_str
@@ -703,7 +703,16 @@ def RegExp.partial_derivative_wrt_str
   (RE : RegExp α)
   (s : Str α) :
   Finset (RegExp α) :=
-  RegExp.partial_derivative_wrt_str_aux {RE} s
+  RegExp.partial_derivative_of_finset_wrt_str_aux {RE} s
+
+
+def RegExp.partial_derivative_of_finset_wrt_str
+  {α : Type}
+  [DecidableEq α]
+  (Γ : Finset (RegExp α))
+  (s : Str α) :
+  Finset (RegExp α) :=
+  RegExp.partial_derivative_of_finset_wrt_str_aux Γ s
 
 
 lemma partial_derivative_wrt_str_aux_last
@@ -712,17 +721,17 @@ lemma partial_derivative_wrt_str_aux_last
   (Γ : Finset (RegExp α))
   (s : Str α)
   (a : α) :
-  RegExp.partial_derivative_wrt_str_aux Γ (s ++ [a]) =
-    RegExp.partial_derivative_of_finset (RegExp.partial_derivative_wrt_str_aux Γ s) a :=
+  RegExp.partial_derivative_of_finset_wrt_str_aux Γ (s ++ [a]) =
+    RegExp.partial_derivative_of_finset_wrt_char (RegExp.partial_derivative_of_finset_wrt_str_aux Γ s) a :=
   by
     induction s generalizing Γ
     case nil =>
       simp
-      simp only [RegExp.partial_derivative_wrt_str_aux]
+      simp only [RegExp.partial_derivative_of_finset_wrt_str_aux]
     case cons hd tl ih =>
       simp
-      simp only [RegExp.partial_derivative_wrt_str_aux]
-      exact ih (RegExp.partial_derivative_of_finset Γ hd)
+      simp only [RegExp.partial_derivative_of_finset_wrt_str_aux]
+      exact ih (RegExp.partial_derivative_of_finset_wrt_char Γ hd)
 
 
 lemma partial_derivative_wrt_str_last
@@ -732,7 +741,7 @@ lemma partial_derivative_wrt_str_last
   (s : Str α)
   (a : α) :
   RegExp.partial_derivative_wrt_str RE (s ++ [a]) =
-    RegExp.partial_derivative_of_finset (RegExp.partial_derivative_wrt_str RE s) a :=
+    RegExp.partial_derivative_of_finset_wrt_char (RegExp.partial_derivative_wrt_str RE s) a :=
   by
     simp only [RegExp.partial_derivative_wrt_str]
     exact partial_derivative_wrt_str_aux_last {RE} s a
@@ -743,7 +752,7 @@ theorem partial_derivative_lang_eq_derivative_lang
   [DecidableEq α]
   (RE : RegExp α)
   (a : α) :
-  finset_regexp_language_of (RE.partial_derivative a) = Language.derivative RE.LanguageOf [a] :=
+  finset_regexp_language_of (RE.partial_derivative_wrt_char a) = Language.derivative RE.LanguageOf [a] :=
   by
     simp only [finset_regexp_language_of]
     induction RE
@@ -751,7 +760,7 @@ theorem partial_derivative_lang_eq_derivative_lang
       simp only [Language.derivative]
       ext cs
       simp
-      simp only [RegExp.partial_derivative]
+      simp only [RegExp.partial_derivative_wrt_char]
       split_ifs
       case pos c1 =>
         simp
@@ -768,24 +777,24 @@ theorem partial_derivative_lang_eq_derivative_lang
     case epsilon =>
       simp only [RegExp.LanguageOf]
       simp only [Language.derivative_of_eps_wrt_char]
-      simp only [RegExp.partial_derivative]
+      simp only [RegExp.partial_derivative_wrt_char]
       simp
     case zero =>
       simp only [RegExp.LanguageOf]
       simp only [Language.derivative_of_empty_wrt_char]
-      simp only [RegExp.partial_derivative]
+      simp only [RegExp.partial_derivative_wrt_char]
       simp
     case union R S R_ih S_ih =>
       simp only [RegExp.LanguageOf]
       simp only [Language.derivative_of_union_wrt_char]
-      simp only [RegExp.partial_derivative]
+      simp only [RegExp.partial_derivative_wrt_char]
       simp only [Finset.set_biUnion_union]
       rw [R_ih]
       rw [S_ih]
     case concat R S R_ih S_ih =>
       simp only [RegExp.LanguageOf]
       simp only [Language.derivative_of_concat_wrt_char]
-      simp only [RegExp.partial_derivative]
+      simp only [RegExp.partial_derivative_wrt_char]
 
       split_ifs
       case pos c1 =>
@@ -831,7 +840,7 @@ theorem partial_derivative_lang_eq_derivative_lang
     case kleene_closure R R_ih =>
       simp only [RegExp.LanguageOf]
       simp only [Language.derivative_of_kleene_closure_wrt_char]
-      simp only [RegExp.partial_derivative]
+      simp only [RegExp.partial_derivative_wrt_char]
       simp only [concat_finset_regexp_regexp]
       simp
       simp only [RegExp.LanguageOf]
@@ -839,15 +848,34 @@ theorem partial_derivative_lang_eq_derivative_lang
       rw [R_ih]
 
 
+theorem partial_derivative_wrt_char_lang_eq_derivative_lang_wrt_char
+  {α : Type}
+  [DecidableEq α]
+  (Γ : Finset (RegExp α))
+  (a : α) :
+  finset_regexp_language_of (RegExp.partial_derivative_of_finset_wrt_char Γ a) = Language.derivative (finset_regexp_language_of Γ) [a] :=
+  by
+    simp only [finset_regexp_language_of]
+    simp only [← Language.derivative_distrib_union_of_finset_wrt_str]
+    simp only [RegExp.partial_derivative_of_finset_wrt_char]
+    simp
+    sorry
+
+
 theorem partial_derivative_wrt_str_lang_eq_derivative_lang_wrt_str
   {α : Type}
   [DecidableEq α]
-  (RE : RegExp α)
+  (Γ : Finset (RegExp α))
   (s : Str α) :
-  finset_regexp_language_of (RE.partial_derivative_wrt_str s) = Language.derivative RE.LanguageOf s :=
+  finset_regexp_language_of (RegExp.partial_derivative_of_finset_wrt_str Γ s) = Language.derivative (finset_regexp_language_of Γ) s :=
   by
-    induction s
+    induction s generalizing Γ
     case nil =>
-      sorry
+      simp only [RegExp.partial_derivative_of_finset_wrt_str]
+      simp only [RegExp.partial_derivative_of_finset_wrt_str_aux]
+      simp only [Language.derivative_wrt_eps]
     case cons hd tl ih =>
+      simp only [RegExp.partial_derivative_of_finset_wrt_str] at *
+      simp only [RegExp.partial_derivative_of_finset_wrt_str_aux]
+      rw [Language.derivative_wrt_cons]
       sorry
