1bc7bccc4b64dbc1273a2a8a440ccaa2d93c94a8
Patrick Thomas <pthomas505@gmail.com>
2024-12-01 15:05:25 -0800

    Updates.

--- FOL/NV/Sub/Var/One/Rec/ReplaceVar.lean
+++ FOL/NV/Sub/Var/One/Rec/ReplaceVar.lean
@@ -111,6 +111,61 @@ theorem not_var_occurs_in_replace_var_one_rec_self
     rw [phi_ih h1_right]
 
 
+theorem replace_var_one_rec_inverse
+  (F : Formula_)
+  (v t : VarName_)
+  (h1 : ¬ var_occurs_in t F) :
+  replace_var_one_rec t v (replace_var_one_rec v t F) = F :=
+  by
+  induction F
+  any_goals
+    simp only [var_occurs_in] at h1
+
+    simp only [replace_var_one_rec]
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+    congr!
+    simp
+    simp only [List.map_eq_self_iff]
+    simp
+    intro x a1
+    by_cases c1 : v = x
+    · simp only [if_pos c1]
+      simp
+      exact c1
+    · simp only [if_neg c1]
+      simp
+      intro a2
+      subst a2
+      contradiction
+  case eq_ x y =>
+      congr!
+      · split_ifs <;> tauto
+      · split_ifs <;> tauto
+  case not_ phi phi_ih =>
+    congr!
+    exact phi_ih h1
+  case
+      imp_ phi psi phi_ih psi_ih
+    | and_ phi psi phi_ih psi_ih
+    | or_ phi psi phi_ih psi_ih
+    | iff_ phi psi phi_ih psi_ih =>
+      congr! <;> tauto
+  case forall_ x phi phi_ih | exists_ x phi phi_ih =>
+    push_neg at h1
+    obtain ⟨h1_left, h1_right⟩ := h1
+
+    split_ifs
+    case pos c1 =>
+      simp only [replace_var_one_rec]
+      simp
+      tauto
+    case neg c1 =>
+      simp only [replace_var_one_rec]
+      simp only [if_neg h1_left]
+      congr!
+      exact phi_ih h1_right
+
+
 theorem not_var_occurs_in_replace_var_one_rec
   (F : Formula_)
   (v t : VarName_)
