1a078580b5280111b186d68083f38bdef603500a
Patrick Thomas <pthomas505@gmail.com>
2024-12-01 11:50:51 -0800

    Updates.

--- FOL/NV/Sub/Var/One/Rec/ReplaceVar.lean
+++ FOL/NV/Sub/Var/One/Rec/ReplaceVar.lean
@@ -40,3 +40,28 @@ def replace_var_one_rec (v t : VarName_) : Formula_ → Formula_
       def_
       X
       (xs.map fun (x : VarName_) => if v = x then t else x)
+
+
+theorem replace_var_one_rec_self
+  (F : Formula_)
+  (v : VarName_) :
+  replace_var_one_rec v v F = F :=
+  by
+  induction F
+  any_goals
+    simp only [replace_var_one_rec]
+  case pred_const_ X xs | pred_var_ X xs | def_ X xs =>
+    simp
+    simp only [List.map_eq_self_iff]
+    simp
+  case eq_ x y =>
+    simp
+  case not_ phi phi_ih =>
+    congr!
+  case
+      imp_ phi psi phi_ih psi_ih
+    | and_ phi psi phi_ih psi_ih
+    | or_ phi psi phi_ih psi_ih
+    | iff_ phi psi phi_ih psi_ih =>
+    congr!
+  case forall_ x phi phi_ih | exists_ x phi phi_ih =>
