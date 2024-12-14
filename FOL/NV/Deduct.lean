3d7003f9062584b9dcc5fa639d69d249b16056e2
Patrick Thomas <pthomas505@gmail.com>
2024-12-13 19:17:20 -0800

    Updates.

--- FOL/NV/Deduct.lean
+++ FOL/NV/Deduct.lean
@@ -11,21 +11,21 @@ open Formula_
   `is_prop_axiom F` := True if and only if `F` is an axiom of classical propositional logic.
 -/
 inductive is_prop_axiom : Formula_ → Prop
-  -- ⊢ ⊤
+  -- `⊢ ⊤`
   | prop_true_ :
     is_prop_axiom true_
 
-  -- ⊢ phi → (psi → phi)
+  -- `⊢ phi → (psi → phi)`
   | prop_1_
     (phi psi : Formula_) :
     is_prop_axiom (phi.imp_ (psi.imp_ phi))
 
-  -- ⊢ (phi → (psi → chi)) → ((phi → psi) → (phi → chi))
+  -- `⊢ (phi → (psi → chi)) → ((phi → psi) → (phi → chi))`
   | prop_2_
     (phi psi chi : Formula_) :
     is_prop_axiom ((phi.imp_ (psi.imp_ chi)).imp_ ((phi.imp_ psi).imp_ (phi.imp_ chi)))
 
-  -- ⊢ (¬ phi → ¬ psi) → (psi → phi)
+  -- `⊢ (¬ phi → ¬ psi) → (psi → phi)`
   | prop_3_
     (phi psi : Formula_) :
     is_prop_axiom (((not_ phi).imp_ (not_ psi)).imp_ (psi.imp_ phi))
@@ -78,32 +78,32 @@ def is_prop_proof (phi : Formula_) : Prop :=
   `is_axiom_v1 F` := True if and only if `F` is an axiom of classical first order logic.
 -/
 inductive is_axiom_v1 : Formula_ → Prop
-  -- ⊢ ⊤
+  -- `⊢ ⊤`
   | prop_true_ :
     is_axiom_v1 true_
 
-  -- ⊢ phi → (psi → phi)
+  -- `⊢ phi → (psi → phi)`
   | prop_1_
     (phi psi : Formula_) :
     is_axiom_v1 (phi.imp_ (psi.imp_ phi))
 
-  -- ⊢ (phi → (psi → chi)) → ((phi → psi) → (phi → chi))
+  -- `⊢ (phi → (psi → chi)) → ((phi → psi) → (phi → chi))`
   | prop_2_
     (phi psi chi : Formula_) :
     is_axiom_v1 ((phi.imp_ (psi.imp_ chi)).imp_ ((phi.imp_ psi).imp_ (phi.imp_ chi)))
 
-  -- ⊢ (¬ phi → ¬ psi) → (psi → phi)
+  -- `⊢ (¬ phi → ¬ psi) → (psi → phi)`
   | prop_3_
     (phi psi : Formula_) :
     is_axiom_v1 (((not_ phi).imp_ (not_ psi)).imp_ (psi.imp_ phi))
 
-  -- ⊢ (∀ v (phi → psi)) → ((∀ v phi) → (∀ v psi))
+  -- `⊢ (∀ v (phi → psi)) → ((∀ v phi) → (∀ v psi))`
   | pred_1_
     (v : VarName_)
     (phi psi : Formula_) :
     is_axiom_v1 ((forall_ v (phi.imp_ psi)).imp_ ((forall_ v phi).imp_ (forall_ v psi)))
 
-  -- ⊢ (∀ v phi) → phi(t/v)  provided phi admits t for v
+  -- `⊢ (∀ v phi) → phi(t/v)` provided `phi` admits `t` for `v`
   | pred_2_
     (v t : VarName_)
     (phi phi' : Formula_) :
@@ -111,19 +111,19 @@ inductive is_axiom_v1 : Formula_ → Prop
     FOL.NV.Sub.Var.One.Rec.fast_replace_free_var_one_rec v t phi = phi' →
     is_axiom_v1 ((forall_ v phi).imp_ phi')
 
-  -- ⊢ phi → (∀ v phi)  provided v is not free in phi
+  -- `⊢ phi → (∀ v phi)` provided `v` is not free in `phi`
   | pred_3_
     (v : VarName_)
     (phi : Formula_) :
     ¬ var_is_free_in v phi →
     is_axiom_v1 (phi.imp_ (forall_ v phi))
 
-  -- ⊢ ∀ v (v = v)
+  -- `⊢ ∀ v (v = v)`
   | eq_1_ (v : VarName_) :
     is_axiom_v1 (forall_ v (eq_ v v))
 
   /-
-    ⊢ ∀ x_0 ... ∀ x_n ∀ y_0 ... y_n ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →((pred_ name [x_0 ... x_n] ↔ pred_ name [y_0 ... y_n]))
+    `⊢ ∀ x_0 ... ∀ x_n ∀ y_0 ... y_n ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →((pred_ name [x_0 ... x_n] ↔ pred_ name [y_0 ... y_n]))`
   -/
   | eq_2_pred_const_
     (name : PredName_)
@@ -136,7 +136,7 @@ inductive is_axiom_v1 : Formula_ → Prop
             ((pred_const_ name (List.ofFn xs)).iff_ (pred_const_ name (List.ofFn ys))))))
 
   /-
-    ⊢ ∀ x_0 ∀ x_1 ∀ y_0 ∀ y_1 ((x_0 = y_0) ∧ (x_1 = y_1)) → ((eq_ x_0 x_1) ↔ (eq_ y_0 y_1))
+    `⊢ ∀ x_0 ∀ x_1 ∀ y_0 ∀ y_1 ((x_0 = y_0) ∧ (x_1 = y_1)) → ((eq_ x_0 x_1) ↔ (eq_ y_0 y_1))`
   -/
   | eq_2_eq_
     (x_0 x_1 y_0 y_1 : VarName_) :
@@ -148,7 +148,7 @@ inductive is_axiom_v1 : Formula_ → Prop
               ((and_ (eq_ x_0 y_0) (eq_ x_1 y_1)).imp_
                 ((eq_ x_0 x_1).iff_ (eq_ y_0 y_1)))))))
 
-  -- ⊢ phi ⇒ ⊢ ∀ v phi
+  -- `⊢ phi ⇒ ⊢ ∀ v phi`
   | gen_
     (v : VarName_)
     (phi : Formula_) :
@@ -211,29 +211,29 @@ def is_proof_v1 (F : Formula_) : Prop :=
 -/
 inductive is_proof_v2 : Formula_ → Prop
 
-  -- ⊢ ⊤
+  -- `⊢ ⊤`
   | prop_true_ : is_proof_v2 true_
 
-  -- ⊢ phi → (psi → phi)
+  -- `⊢ phi → (psi → phi)`
   | prop_1_
     (phi psi : Formula_) :
     is_proof_v2 (phi.imp_ (psi.imp_ phi))
 
-  -- ⊢ (phi → (psi → chi)) → ((phi → psi) → (phi → chi))
+  -- `⊢ (phi → (psi → chi)) → ((phi → psi) → (phi → chi))`
   | prop_2_
     (phi psi chi : Formula_) :
     is_proof_v2
       ((phi.imp_ (psi.imp_ chi)).imp_
         ((phi.imp_ psi).imp_ (phi.imp_ chi)))
 
-  -- ⊢ (¬ phi → ¬ psi) → (psi → phi)
+  -- `⊢ (¬ phi → ¬ psi) → (psi → phi)`
   | prop_3_
     (phi psi : Formula_) :
     is_proof_v2
       (((not_ phi).imp_ (not_ psi)).imp_
         (psi.imp_ phi))
 
-  -- ⊢ (∀ v (phi → psi)) → ((∀ v phi) → (∀ v psi))
+  -- `⊢ (∀ v (phi → psi)) → ((∀ v phi) → (∀ v psi))`
   | pred_1_
     (v : VarName_) (phi psi : Formula_) :
     is_proof_v2
@@ -241,27 +241,27 @@ inductive is_proof_v2 : Formula_ → Prop
         ((forall_ v phi).imp_
           (forall_ v psi)))
 
-  -- ⊢ (∀ v phi) → phi(t/v)  provided phi admits t for v
+  -- `⊢ (∀ v phi) → phi(t/v)` provided `phi` admits `t` for `v`
   | pred_2_
     (v t : VarName_) (phi phi' : Formula_) :
     FOL.NV.Sub.Var.One.Rec.fast_admits_var_one_rec v t phi →
       FOL.NV.Sub.Var.One.Rec.fast_replace_free_var_one_rec v t phi = phi' →
         is_proof_v2 ((forall_ v phi).imp_ phi')
 
-  -- ⊢ phi → (∀ v phi)  provided v is not free in phi
+  -- `⊢ phi → (∀ v phi)` provided `v` is not free in `phi`
   | pred_3_
     (v : VarName_)
     (phi : Formula_) :
     ¬ var_is_free_in v phi →
     is_proof_v2 (phi.imp_ (forall_ v phi))
 
-  -- ⊢ ∀ v (v = v)
+  -- `⊢ ∀ v (v = v)`
   | eq_1_
     (v : VarName_) :
     is_proof_v2 (forall_ v (eq_ v v))
 
   /-
-    ⊢ ∀ x_0 ... ∀ x_n ∀ y_0 ... y_n ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →((pred_ name [x_0 ... x_n] ↔ pred_ name [y_0 ... y_n]))
+    `⊢ ∀ x_0 ... ∀ x_n ∀ y_0 ... y_n ((x_0 = y_0) ∧ ... ∧ (x_n = y_n) ∧ ⊤) →((pred_ name [x_0 ... x_n] ↔ pred_ name [y_0 ... y_n]))`
   -/
   | eq_2_pred_const_
     (name : PredName_)
@@ -274,7 +274,7 @@ inductive is_proof_v2 : Formula_ → Prop
             ((pred_const_ name (List.ofFn xs)).iff_ (pred_const_ name (List.ofFn ys))))))
 
   /-
-    ⊢ ∀ x_0 ∀ x_1 ∀ y_0 ∀ y_1 ((x_0 = y_0) ∧ (x_1 = y_1)) → ((eq_ x_0 x_1) ↔ (eq_ y_0 y_1))
+    `⊢ ∀ x_0 ∀ x_1 ∀ y_0 ∀ y_1 ((x_0 = y_0) ∧ (x_1 = y_1)) → ((eq_ x_0 x_1) ↔ (eq_ y_0 y_1))`
   -/
   | eq_2_eq_
     (x_0 x_1 y_0 y_1 : VarName_) :
@@ -286,14 +286,14 @@ inductive is_proof_v2 : Formula_ → Prop
               ((and_ (eq_ x_0 y_0) (eq_ x_1 y_1)).imp_
                 ((eq_ x_0 x_1).iff_ (eq_ y_0 y_1)))))))
 
-  -- ⊢ phi ⇒ ⊢ ∀ v phi
+  -- `⊢ phi ⇒ ⊢ ∀ v phi`
   | gen_
     (v : VarName_)
     (phi : Formula_) :
     is_proof_v2 phi →
     is_proof_v2 (forall_ v phi)
 
-  -- ⊢ phi → psi ⇒ ⊢ phi ⇒ ⊢ psi
+  -- `⊢ phi → psi ⇒ ⊢ phi ⇒ ⊢ psi`
   | mp_
     (phi psi : Formula_) :
     is_proof_v2 (phi.imp_ psi) →
