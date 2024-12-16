ba48da4cb8b3c3c76250bb5b1a10b7db78131bcd
Patrick Thomas <pthomas505@gmail.com>
2024-12-15 21:08:53 -0800

    Updates.

--- FOL/NV/Prop.lean
+++ FOL/NV/Prop.lean
@@ -233,18 +233,18 @@ example
 
 
 theorem T_13_5
-  (F : Formula_) :
-  is_proof_v1 (F.imp_ F) :=
+  (P : Formula_) :
+  is_proof_v1 (P.imp_ P) :=
   by
   simp only [is_proof_v1]
-  apply is_deduct_v1.mp_ (F.imp_ (F.imp_ F))
-  · apply is_deduct_v1.mp_ (F.imp_ ((F.imp_ F).imp_ F))
+  apply is_deduct_v1.mp_ (P.imp_ (P.imp_ P))
+  · apply is_deduct_v1.mp_ (P.imp_ ((P.imp_ P).imp_ P))
     · apply is_deduct_v1.axiom_
-      exact is_axiom_v1.prop_2_ F (F.imp_ F) F
+      exact is_axiom_v1.prop_2_ P (P.imp_ P) P
     · apply is_deduct_v1.axiom_
-      exact is_axiom_v1.prop_1_ F (F.imp_ F)
+      exact is_axiom_v1.prop_1_ P (P.imp_ P)
   · apply is_deduct_v1.axiom_
-    exact is_axiom_v1.prop_1_ F F
+    exact is_axiom_v1.prop_1_ P P
 
 alias prop_id := T_13_5
 
